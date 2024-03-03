b3e9c6cd3cdbe712bc708a9e878c58f600f44bd6
Juno Nguyen <juno.ngx@gmail.com>
2024-03-03 19:14:13 +0800

    Re-template item

--- src/components/ItemComponent.tsx
+++ src/components/ItemComponent.tsx
@@ -2,7 +2,14 @@ import { Item } from "../types";
 
 export default function ItemComponent({ item }: { item: Item }) {
 
-    return <div>
-        title: {item.title} | content: {item.content} | type: {item.type}
+    // return <div>
+    //     title: {item.title} | content: {item.content} | type: {item.type}
+    // </div>
+
+    return <div className="item">
+        <div className="item__icon">icon</div>
+        <div className="item__primary-text">{item.title}</div>
+        <div className="item__secondary-text">{item.content}</div>
+        <div className="item__updated_date">{item.updated}</div>
     </div>
 };
