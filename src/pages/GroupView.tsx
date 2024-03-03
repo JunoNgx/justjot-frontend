c1007d7701bd7496ffa33d91eb89da778069e0c6
Juno Nguyen <juno.ngx@gmail.com>
2024-03-03 17:44:56 +0800

    Draft fetching data from backend

--- src/pages/GroupView.tsx
+++ src/pages/GroupView.tsx
@@ -1,3 +1,21 @@
+import PocketBase from 'pocketbase';
+import { RecordModel } from 'pocketbase';
+
+import { useEffect, useState } from "react";
+
 export default function GroupView() {
+    const [list, setList] = useState<RecordModel[]>();
+
+    useEffect(() => {
+        const fetch = async () => {
+            const pbClient = new PocketBase(import.meta.env.VITE_BACKEND_URL);
+            const records = await pbClient.collection("items").getFullList();
+            setList(records)
+        };
+
+        fetch();
+        console.log(list)
+    }, []);
+    
     return <h1>Main item list view</h1>
 }
