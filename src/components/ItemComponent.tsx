04005b94d8874969c52cc4cabdd269c82d5a121f
Juno Nguyen <juno.ngx@gmail.com>
2024-03-03 20:27:52 +0800

    Implement template data calculation

--- src/components/ItemComponent.tsx
+++ src/components/ItemComponent.tsx
@@ -10,26 +10,34 @@ export default function ItemComponent({ item }: { item: Item }) {
 
     const isTodoItem = item.type === ItemType.TODO;
 
-    const datetimeDataToUse = item.updated || item.created;
-    const dateTimeInstance = new Date(datetimeDataToUse);
-    const formatOptions: Intl.DateTimeFormatOptions = {
-        day: "numeric",
-        month: "short",
-        year: "2-digit",
+    const calculatePriText = (): string => {
+        return isTodoItem
+            ? item.content
+            : item.title;
     };
-    // TODO; const isMoreThanOneYear
-    const dateTimeStr = Intl.DateTimeFormat(
-        "en-US", formatOptions).format(dateTimeInstance);
-    
+
+    const calculateDatetimeStr = (): string => {
+        const datetimeDataToUse = item.updated || item.created;
+        const dateTimeInstance = new Date(datetimeDataToUse);
+        const formatOptions: Intl.DateTimeFormatOptions = {
+            day: "numeric",
+            month: "short",
+            year: "2-digit",
+        };
+        // TODO; const isMoreThanOneYear
+        return Intl.DateTimeFormat("en-US", formatOptions)
+            .format(dateTimeInstance);
+    };
 
     return <div className="item">
         <div className="item__left-side">
             <div className="item__icon">icon</div>
-            {!isTodoItem && <div className="item__primary-text">{item.title}</div>}
-            <div className="item__secondary-text">{item.content}</div>
+            <div className="item__primary-text">{calculatePriText()}</div>
+            {!isTodoItem &&
+                <div className="item__secondary-text">{item.content}</div>}
         </div>
         <div className="item__right-side">
-            <div className="item__updated_date">{dateTimeStr}</div>
+            <div className="item__datetime">{calculateDatetimeStr()}</div>
         </div>
     </div>
 };
