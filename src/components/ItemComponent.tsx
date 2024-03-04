6e46111fc1b400cd8f72e2f951882c3fae06405c
Juno Nguyen <juno.ngx@gmail.com>
2024-03-04 20:16:43 +0800

    Implement is active state

--- src/components/ItemComponent.tsx
+++ src/components/ItemComponent.tsx
@@ -1,5 +1,6 @@
 // import styled from 'styled-components';
 
+import { useState } from "react";
 import { Item, ItemType } from "../types";
 import { calculatePriText, calculateDatetimeStr } from "../utils/itemUtils";
 
@@ -8,11 +9,19 @@ export default function ItemComponent({ item }: { item: Item }) {
     // return <div>
     //     title: {item.title} | content: {item.content} | type: {item.type}
     // </div>
-
+    const [isFocused, setIsFocused] = useState(false);
     const isTodoItem = item.type === ItemType.TODO;
+    // const setIsFocused = (val: boolean) => {
+    //     console.log("set is focused", val)
+    //     isFocused = val;
+    // }
 
-    return <div className="item">
+    return <div className={"item " + (isFocused ? "item--is-active" : "")}
+        onMouseEnter={() => { setIsFocused(true) }}
+        onMouseLeave={() => { setIsFocused(false) }}
+    >
         <div className="item__left-side">
+            {isFocused}
             <div className="item__icon">icon</div>
             <div className="item__primary-text">{calculatePriText(item)}</div>
             {!isTodoItem &&
