62a6f1fa1023155b53010c1f7dd1263039bb4ac3
Juno Nguyen <juno.ngx@gmail.com>
2024-03-04 23:05:19 +0800

    Implement ui for login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,27 @@
 export default function Login() {
     return <div className="login-wrapper">
         <div className="login">
-            Login route
+            <h2>Login</h2>
+
+            <form className="login__form">
+                <div className="login__field">
+                    <label>
+                        <div className="login__label">Email</div>
+                        <input className="login__input" required type="email"></input>
+                    </label>
+                </div>
+                <div className="login__field">
+                    <label>
+                        <div className="login__label">Password</div>
+                        <input className="login__input" required type="password"></input>
+                    </label>
+                </div>
+                <div className="login__button-wrapper">
+                    <button className="login__button" type="submit">
+                        Login
+                    </button>
+                </div>
+            </form>
         </div>
     </div>
 }
