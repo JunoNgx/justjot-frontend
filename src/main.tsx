1a26f4a605de6098a996ee84e086af6a3b65bfea
Juno Nguyen <juno.ngx@gmail.com>
2024-03-06 01:02:52 +0800

    Draft theming

--- src/main.tsx
+++ src/main.tsx
@@ -3,16 +3,27 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
 
-import { createTheme, MantineProvider } from '@mantine/core';
+import {
+    createTheme,
+    DEFAULT_THEME,
+    mergeMantineTheme,
+    MantineProvider,
+} from '@mantine/core';
+
 import '@mantine/core/styles.css';
 
-const theme = createTheme({
-  /** Your theme override here */
+const overrideTheme = createTheme({
+    primaryColor: "teal",
+    // fontFamily: "",
+    // fontFamilyMonospace: "",
+    defaultRadius: 0,
 });
+const mergedTheme = mergeMantineTheme(DEFAULT_THEME, overrideTheme);
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
-        <MantineProvider theme={theme}>
+        <MantineProvider theme={mergedTheme}>
             <React.StrictMode>
                 <App />
             </React.StrictMode>
