55916482e55837b39361db47bedf4bd586cf2a49
Juno Nguyen <juno.ngx@gmail.com>
2024-03-06 00:31:23 +0800

    Setup MantimeProvider

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,19 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
 
+import { createTheme, MantineProvider } from '@mantine/core';
+import '@mantine/core/styles.css';
+
+const theme = createTheme({
+  /** Your theme override here */
+});
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
-        <React.StrictMode>
-            <App />
-        </React.StrictMode>
+        <MantineProvider theme={theme}>
+            <React.StrictMode>
+                <App />
+            </React.StrictMode>
+        </MantineProvider>
     </BrowserRouter>
 )
