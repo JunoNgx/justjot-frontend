e01d56648d44c866c629c3241a5a4466bd2cd890
Juno Nguyen <juno.ngx@gmail.com>
2024-03-07 21:09:18 +0800

    Relayout main with mantine

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 // import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Routes, Route } from "react-router-dom";
 
+import { AppShell, Center } from '@mantine/core';
+
 import Header from "./components/Header";
 
 import LandingPage from "./pages/LandingPage";
@@ -13,22 +15,33 @@ import Register from "./pages/Register";
 import "./styles/main.scss";
 
 function App() {
-    return <div className="app-container">
-        <Header/>
-        <main>
-            <Routes>
-                <Route path="/" element={<LandingPage />}/>
-                <Route path="/help" element={<Help />}/>
-                <Route path="/:username">
-                    <Route index element={<GroupView/>}/>
-                    <Route path=":groupSlug" element={<GroupView />}/>
-                </Route>
-                <Route path="/login" element={<Login />}/>
-                <Route path="/register" element={<Register />}/>
-                <Route path="/*" element={<Redirect />}/>
-            </Routes>
-        </main>
-    </div>
+    // return <div className="app-container">
+    return <AppShell
+                header={{ height: 60}}
+                padding="xl"
+            >
+                <AppShell.Header>
+                    <Header/>
+                </AppShell.Header>
+
+                <AppShell.Main>
+                    <Center>
+                        <Routes>
+                            <Route path="/" element={<LandingPage />} />
+                            <Route path="/help" element={<Help />} />
+                            <Route path="/:username">
+                                <Route index element={<GroupView />} />
+                                <Route path=":groupSlug" element={<GroupView />} />
+                            </Route>
+                            <Route path="/login" element={<Login />} />
+                            <Route path="/register" element={<Register />} />
+                            <Route path="/*" element={<Redirect />} />
+                        </Routes>
+                    </Center>
+                </AppShell.Main>
+
+        </AppShell>
+    // </div>
 }
 
 export default App;
