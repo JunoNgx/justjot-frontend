8536043bcc1e47e4fba08f4ecf873f2ecb182c92
Juno Nguyen <juno.ngx@gmail.com>
2024-03-09 13:16:55 +0800

    Add navlinks to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,9 @@
 import { useContext, useEffect } from "react";
-import { Button, Group, Text, Title } from "@mantine/core";
+import { Button, Container, Group, Text, Title } from "@mantine/core";
 import { ThemeModeContext } from "../contexts/ThemeModeContext";
 import { ThemeMode } from "../types";
 import { BackendClientContext } from "../contexts/BackendClientContext";
+import { NavLink } from "react-router-dom";
 
 function Header() {
 
@@ -13,52 +14,66 @@ function Header() {
     //     console.log(pbClient)
     // }, []);
 
-    return <Group
-        className="header"
-        justify="space-between"
-    >
-        <div className="header__left-side">
-            <Title
-                order={1}
-            >
-                JustJot
-            </Title>
-        </div>
-
-        <Group
-            className="header__right-side"
-            gap="md"
-        >
-            <Group className="header__theme-mode-container"
-                gap="sm"
-            >
-                <Button
-                    className="header__theme-mode-option"
-                    onClick={() => {setThemeMode(ThemeMode.AUTO)}}
-                >
-                    Auto
-                </Button>
-                <Button
-                    className="header__theme-mode-option"
-                    onClick={() => {setThemeMode(ThemeMode.LIGHT)}}
-                >
-                    Light
-                </Button>
-                <Button
-                    className="header__theme-mode-option"
-                    onClick={() => {setThemeMode(ThemeMode.DARK)}}
-                >
-                    Dark
-                </Button>
-            </Group>
-            {/* <Text>
-                Login / Register
-            </Text> */}
-            <Text>
-                Is logged in: {pbClient.authStore.isValid.toString()}
-            </Text>
-        </Group>
+    // const rightCorner = () => pbClient.authStore.isValid
+    //     ? LoginRegisterLink
+    //     : UsernameDropdown
+    
+    const LoginRegisterLink = <Group>
+        <NavLink to="login">Login</NavLink>
+        <Text>/</Text>
+        <NavLink to="register">Register</NavLink>
     </Group>
+
+    const usernameDropdownMenu = <Group>
+        Username
+    </Group>
+
+    return <Container>
+        <Group
+            className="header"
+            justify="space-between"
+        >
+            <div className="header__left-side">
+                <Title
+                    order={1}
+                >
+                    JJ
+                </Title>
+            </div>
+
+            <Group
+                className="header__right-side"
+                gap="md"
+            >
+                <Group className="header__theme-mode-container"
+                    gap="sm"
+                >
+                    <Button
+                        className="header__theme-mode-option"
+                        onClick={() => {setThemeMode(ThemeMode.AUTO)}}
+                    >
+                        Auto
+                    </Button>
+                    <Button
+                        className="header__theme-mode-option"
+                        onClick={() => {setThemeMode(ThemeMode.LIGHT)}}
+                    >
+                        Light
+                    </Button>
+                    <Button
+                        className="header__theme-mode-option"
+                        onClick={() => {setThemeMode(ThemeMode.DARK)}}
+                    >
+                        Dark
+                    </Button>
+                </Group>
+
+                {LoginRegisterLink}
+                {/* {usernameDropdownMenu} */}
+
+            </Group>
+        </Group>
+    </Container>
 }
 
 export default Header;
