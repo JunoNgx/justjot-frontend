03dbf593d28617f070a45805e3392425c6fdb3c3
Juno Nguyen <juno.ngx@gmail.com>
2024-03-10 15:31:19 +0800

    Implement icon for item

--- src/components/ItemComponent.tsx
+++ src/components/ItemComponent.tsx
@@ -3,40 +3,53 @@
 import { useState } from "react";
 import { Item, ItemType } from "../types";
 import { calculatePriText, processDatetime } from "../utils/itemUtils";
-// import { DateTime } from "luxon";
+import { Group, Paper, Text } from "@mantine/core";
+import { IconClipboardText, IconNote, IconWorld } from "@tabler/icons-react";
 
 export default function ItemComponent({ item }: { item: Item }) {
 
-    // return <div>
-    //     title: {item.title} | content: {item.content} | type: {item.type}
-    // </div>
     const [isFocused, setIsFocused] = useState(false);
-    const isTodoItem = item.type === ItemType.TODO;
-    // const setIsFocused = (val: boolean) => {
-    //     console.log("set is focused", val)
-    //     isFocused = val;
-    // }
-
+    // const isTodoItem = item.type === ItemType.TODO;
     const { relativeDatetime, fullDatetime } = processDatetime(item);
+    const hasBorder = isFocused;
+    const icon = computeIcon(item);
 
-    return <div className={"item " + (isFocused ? "item--is-active" : "")}
+    return <Paper className={"item " + (isFocused ? "item--is-active" : "")}
+        withBorder={hasBorder}
+        p="xs"
         onMouseEnter={() => { setIsFocused(true) }}
         onMouseLeave={() => { setIsFocused(false) }}
     >
-        <div className="item__left-side">
-            {isFocused}
-            <div className="item__icon">icon</div>
-            <div className="item__primary-text">{calculatePriText(item)}</div>
-            {!isTodoItem &&
-                <div className="item__secondary-text">{item.content}</div>}
-        </div>
-        <div className="item__right-side">
-            <div
-                className="item__datetime"
-                title={fullDatetime}
-            >
-                {relativeDatetime}
-            </div>
-        </div>
-    </div>
+        <Group className="item__flex-wrapper"
+            justify="space-between"
+
+        >
+            <Group className="item__left-side">
+                {/* {isFocused} */}
+                <div className="item__icon">{icon}</div>
+                <div className="item__primary-text">{calculatePriText(item)}</div>
+                <div className="item__secondary-text">{item.content}</div>
+            </Group>
+            <Group className="item__right-side">
+                <Text
+                    className="item__datetime"
+                    title={fullDatetime}
+                >
+                    {relativeDatetime}
+                </Text>
+            </Group>
+        </Group>
+    </Paper>
 };
+
+const computeIcon = (item: Item) => {
+    switch (item.type) {
+        case ItemType.LINK:
+            return <IconWorld/>
+        case ItemType.TEXT:
+        default:
+            return item.shouldCopyUponClick
+                ? <IconClipboardText/>
+                : <IconNote/>
+    }
+}
