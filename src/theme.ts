f86de6eb75c65629be0f587f953a4b10c0449eec
Juno Nguyen <juno.ngx@gmail.com>
2024-03-10 14:21:18 +0800

    Add theme variables

--- src/theme.ts
+++ src/theme.ts
@@ -11,6 +11,10 @@ const overrideTheme = createTheme({
     fontFamily: "IBM Plex Sans, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, sans-serif, Segoe UI Emoji", 
     fontFamilyMonospace: "Cousine, Menlo, Consolas, monospace",
     defaultRadius: 0,
+    other: {
+        colText: "#000", // --mantine-colortext; --mantine-color-black
+        colTextDark: "#C9C9C9" // --mantine-color-dark-0
+    }
 });
 
 export const justJotTheme = mergeMantineTheme(DEFAULT_THEME, overrideTheme);
