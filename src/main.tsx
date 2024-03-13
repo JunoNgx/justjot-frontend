802537c5d1c12c1a97ea42c12b0dfd62a5b3249e
Juno Nguyen <juno.ngx@gmail.com>
2024-03-13 21:17:01 +0800

    Add context menu to entry point

--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,25 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
 import { MantineProvider } from '@mantine/core'
+import { ContextMenuProvider } from 'mantine-contextmenu';
+
+import "@mantine/core/styles.css";
+import "mantine-contextmenu/styles.css";
+
+import '@mantine/core/styles.layer.css';
+import 'mantine-contextmenu/styles.layer.css';
+import './styles/layers.css';
 
 import { justJotTheme } from './theme.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
         <MantineProvider theme={justJotTheme}>
-            <React.StrictMode>
-                <App />
-            </React.StrictMode>
+            <ContextMenuProvider>
+                <React.StrictMode>
+                    <App />
+                </React.StrictMode>
+            </ContextMenuProvider>
         </MantineProvider>
     </BrowserRouter>
 )
