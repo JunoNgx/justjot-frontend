3a93fcd8331e4ef38815630368d68e17540f212d
Juno Nguyen <juno.ngx@gmail.com>
2024-03-13 21:40:47 +0800

    Switch main font to Space Grotesk

--- src/theme.ts
+++ src/theme.ts
@@ -8,7 +8,7 @@ import '@mantine/core/styles.css';
 
 const overrideTheme = createTheme({
     primaryColor: "teal",
-    fontFamily: "IBM Plex Sans, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, sans-serif, Segoe UI Emoji", 
+    fontFamily: "Space Grotesk, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, sans-serif, Segoe UI Emoji", 
     fontFamilyMonospace: "Cousine, Menlo, Consolas, monospace",
     defaultRadius: 0,
     other: {
