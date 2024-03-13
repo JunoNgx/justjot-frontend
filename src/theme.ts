c758517fd41da23574ceca1e2612214be2796453
Juno Nguyen <juno.ngx@gmail.com>
2024-03-13 22:02:42 +0800

    Use Space Mono for secondary text

--- src/theme.ts
+++ src/theme.ts
@@ -9,7 +9,7 @@ import '@mantine/core/styles.css';
 const overrideTheme = createTheme({
     primaryColor: "teal",
     fontFamily: "Space Grotesk, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, sans-serif, Segoe UI Emoji", 
-    fontFamilyMonospace: "Cousine, Menlo, Consolas, monospace",
+    fontFamilyMonospace: "Space Mono, Menlo, Consolas, monospace",
     defaultRadius: 0,
     other: {
         colText: "#000", // --mantine-colortext; --mantine-color-black
