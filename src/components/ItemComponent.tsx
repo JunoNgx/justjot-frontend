185c4be424aeaf125943eeef5102f93ce38ee69d
Juno Nguyen <juno.ngx@gmail.com>
2024-03-16 00:18:27 +0800

    Reimport and use hooks for ItemComponent

--- src/components/ItemComponent.tsx
+++ src/components/ItemComponent.tsx
@@ -1,6 +1,6 @@
 // import styled from 'styled-components';
 
-import { useContext, useState } from "react";
+import { useState } from "react";
 import { Item, ItemType } from "../types";
 import { calculatePriText, processDatetime } from "../utils/itemUtils";
 import { Center, Group, Image, Paper, Text } from "@mantine/core";
@@ -8,58 +8,49 @@ import { IconNote, IconNotes, IconWorld } from "@tabler/icons-react";
 import { isValidUrl } from "../utils/misc";
 import { useContextMenu } from 'mantine-contextmenu';
 import { justJotTheme } from "../theme";
-import { BackendClientContext } from "../contexts/BackendClientContext";
+import useContextMenuActions from "../hooks/useContextMenuActions";
 
 export default function ItemComponent({ item }: { item: Item }) {
 
-    const { fetchItems } = useContext(BackendClientContext)
+    const { showContextMenu } = useContextMenu();
+
     const [isFocused, setIsFocused] = useState(false);
-    // const isTodoItem = item.type === ItemType.TODO;
     const { relativeDatetime, fullDatetime } = processDatetime(item);
     const icon = computeIcon(item);
 
-    const { showContextMenu } = useContextMenu();
-
-    const refetch = async () => {
-        await fetch(`${import.meta.env.VITE_BACKEND_URL}refetch/${item.id}`, {
-            method: "POST"
-        })
-        .then(() => {
-            fetchItems();
-        });
-    }
+    const { refetchTitleAndFavicon } = useContextMenuActions();
+    const contextMenuItems = [
+        {
+            key: "copy",
+            onClick: () => {}
+        }, {
+            key: "edit",
+            onClick: () => {}
+        }, {
+            key: "move",
+            onClick: () => {}
+        }, {
+            key: "refetch link",
+            onClick: () => {refetchTitleAndFavicon(item)}
+        }, {
+            key: "delete",
+            color: "red",
+            onClick: () => {}
+        }, {
+            key: "divider",
+        }, {
+            key: "Copy by default",
+            color: "blue",
+            onClick: () => {}
+        }
+    ]
 
     return <Paper className={"item " + (isFocused ? "item--is-active" : "")}
         p="xs"
         onMouseEnter={() => { setIsFocused(true) }}
         onMouseLeave={() => { setIsFocused(false) }}
-        // onContextMenu={handleContextMenu}
         onContextMenu={showContextMenu(
-            [
-                {
-                    key: "copy",
-                    onClick: () => {}
-                }, {
-                    key: "edit",
-                    onClick: () => {}
-                }, {
-                    key: "delete",
-                    color: "red",
-                    onClick: () => {}
-                }, {
-                    key: "move",
-                    onClick: () => {}
-                }, {
-                    key: "refetch link",
-                    onClick: refetch
-                }, {
-                    key: "divider",
-                }, {
-                    key: "Copy by default",
-                    color: "blue",
-                    onClick: () => {}
-                }
-            ],
+            contextMenuItems,
             { className: "dropdown-menu" }
         )}
     >
