57a80e3d152c45aba5b8489655e169617f1bc437
Juno Nguyen <juno.ngx@gmail.com>
2024-03-16 18:44:39 +0800

    Add AUTO_CLOSE_DEFAULT to constants

--- src/utils/constants.tsx
+++ src/utils/constants.tsx
@@ -1,3 +1,4 @@
 export const SORT_ORDER_INCREMENT_COLLECTION = 1000;
 export const SORT_ORDER_INCREMENT_TODO_ITEM = 1000;
 export const AUTO_CLOSE_ERROR_TOAST = false;
+export const AUTO_CLOSE_DEFAULT = 3000;
