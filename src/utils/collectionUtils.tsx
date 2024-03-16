53de048e99aa4b86e883a1eeb9e62e77ce01a5d7
Juno Nguyen <juno.ngx@gmail.com>
2024-03-16 18:05:42 +0800

    Implement

---
+++ src/utils/collectionUtils.tsx
@@ -0,0 +1,10 @@
+import { ItemCollection } from "../types";
+
+export const getCurrHighestCollectionSortOrder = (collections: ItemCollection[] | undefined) => {
+    if (!collections) return null;
+
+    const collectionCount = collections?.length;
+    const lastCollectionIndex = collectionCount - 1;
+
+    return collections[lastCollectionIndex - 1].sortOrder;
+}
