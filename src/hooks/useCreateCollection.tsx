e344a6bd57c3fee76d1b3454d343b823e30f4b10
Juno Nguyen <juno.ngx@gmail.com>
2024-03-16 17:57:36 +0800

    Add sortOrder to createCollection

--- src/hooks/useCreateCollection.tsx
+++ src/hooks/useCreateCollection.tsx
@@ -2,9 +2,13 @@ import { useContext, useState } from "react";
 import { BackendClientContext } from "../contexts/BackendClientContext";
 import { DbTable, ItemCollection, CreateUpdateCollectionOptions, RequestCallbackOptions } from "../types";
 import { notifications } from "@mantine/notifications";
+import { SORT_ORDER_INCREMENT_COLLECTION } from "../utils/constants";
 
 type useCreateCollectionReturnType = [
-    ({ name, slug }: CreateUpdateCollectionOptions) => Promise<void>,
+    (
+        { name, slug }: CreateUpdateCollectionOptions,
+        currHighestSortOrder?: number
+    ) => Promise<void>,
     boolean,
     boolean,
 ];
@@ -17,10 +21,20 @@ export default function useCreateCollection(
     const [isSuccessful, setIsSuccessful] = useState(false);
     const [isLoading, setIsLoading] = useState(false);
 
-    const createCollection = async ({ name, slug }: CreateUpdateCollectionOptions) => {
+    const createCollection = async ({ name, slug }: CreateUpdateCollectionOptions, currHighestSortOrder?: number) => {
+
+        const newSortOrderVal: number = currHighestSortOrder
+            ? currHighestSortOrder + SORT_ORDER_INCREMENT_COLLECTION
+            : 0;
+
         setIsLoading(true);
         await pbClient.collection(DbTable.COLLECTIONS)
-            .create({ name, slug, owner: user!.id })
+            .create({
+                name,
+                slug,
+                owner: user!.id,
+                sortOrder: newSortOrderVal
+            })
             .then((_record: ItemCollection) => {
                 setIsSuccessful(true);
                 successfulCallback?.();
