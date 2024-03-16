4d8fe7b6b963bc3a5becdb4226d1c03aeaef3006
Juno Nguyen <juno.ngx@gmail.com>
2024-03-16 17:11:01 +0800

    Rearrange to allow modals to access backend context

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 // import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Routes, Route } from "react-router-dom";
-
 import { AppShell } from '@mantine/core';
+import { ContextMenuProvider } from 'mantine-contextmenu';
+import { ModalsProvider } from '@mantine/modals';
+
+import ThemeModeContextProvider from "./contexts/ThemeModeContext";
+import BackendClientContextProvider from "./contexts/BackendClientContext";
 
 import Header from "./components/Header";
-
 import LandingPage from "./pages/LandingPage";
 import Help from "./pages/Help";
 import MainView from "./pages/MainView";
@@ -13,10 +16,15 @@ import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Reset from "./pages/Reset";
 
-import "./styles/main.scss";
+import "@mantine/core/styles.css";
+import '@mantine/notifications/styles.css';
+import "mantine-contextmenu/styles.css";
 
-import ThemeModeContextProvider from "./contexts/ThemeModeContext";
-import BackendClientContextProvider from "./contexts/BackendClientContext";
+import '@mantine/core/styles.layer.css';
+import 'mantine-contextmenu/styles.layer.css';
+import './styles/layers.css';
+
+import "./styles/main.scss";
 
 function App() {
 
@@ -27,26 +35,30 @@ function App() {
 
         <ThemeModeContextProvider>
             <BackendClientContextProvider>
+                <ModalsProvider>
+                    <ContextMenuProvider>
 
-                <AppShell.Header>
-                    <Header/>
-                </AppShell.Header>
+                        <AppShell.Header>
+                            <Header/>
+                        </AppShell.Header>
 
-                <AppShell.Main className="appshell-main">
-                    <Routes>
-                        <Route path="/" element={<LandingPage />} />
-                        <Route path="/help" element={<Help />} />
-                        <Route path="/:username">
-                            <Route index element={<MainView />} />
-                            <Route path=":groupSlug" element={<MainView />} />
-                        </Route>
-                        <Route path="/login" element={<Login />} />
-                        <Route path="/register" element={<Register />} />
-                        <Route path="/reset" element={<Reset />} />
-                        <Route path="/*" element={<Redirect />} />
-                    </Routes>
-                </AppShell.Main>
+                        <AppShell.Main className="appshell-main">
+                            <Routes>
+                                <Route path="/" element={<LandingPage />} />
+                                <Route path="/help" element={<Help />} />
+                                <Route path="/:username">
+                                    <Route index element={<MainView />} />
+                                    <Route path=":groupSlug" element={<MainView />} />
+                                </Route>
+                                <Route path="/login" element={<Login />} />
+                                <Route path="/register" element={<Register />} />
+                                <Route path="/reset" element={<Reset />} />
+                                <Route path="/*" element={<Redirect />} />
+                            </Routes>
+                        </AppShell.Main>
 
+                    </ContextMenuProvider>
+                </ModalsProvider>
             </BackendClientContextProvider>
         </ThemeModeContextProvider>
 
