59c28a62d6991241b28050bf6f416f68db13f52a
Juno Nguyen <juno.ngx@gmail.com>
2024-03-17 15:55:10 +0800

    Import main input to main view

--- src/pages/MainView.tsx
+++ src/pages/MainView.tsx
@@ -1,28 +1,18 @@
-import { useContext, useEffect, useState } from "react";
-import { Group, Input, Loader, Stack } from '@mantine/core';
-import ItemComponent from '../components/ItemComponent';
+import { useContext, useEffect } from "react";
+import { Stack } from '@mantine/core';
 import { BackendClientContext } from '../contexts/BackendClientContext';
 import { useNavigate } from 'react-router-dom';
-import { IconCircleTriangle } from '@tabler/icons-react';
-import { justJotTheme } from "../theme";
 import { Notifications } from '@mantine/notifications';
-import useCreateItem from "../hooks/useCreateItem";
+import ItemComponent from '../components/ItemComponent';
+import MainInput from "../components/MainInput";
 
 export default function MainView() {
-
     const {
         isLoggedIn,
         items,
-        fetchItems,
     } = useContext(BackendClientContext);
 
     const navigate = useNavigate();
-    const [createItem, isCreateItemLoading] = useCreateItem({
-        successfulCallback: () => {
-            setInputVal("");
-            fetchItems();
-        }
-    });
 
     useEffect(() => {
         if (!isLoggedIn) {
@@ -30,13 +20,6 @@ export default function MainView() {
             return;
         }
     });
-
-    const [inputVal, setInputVal] = useState("");
-    const handleKeyDown = async (e: React.KeyboardEvent<HTMLInputElement>) => {
-        if (e.code === "Enter") {
-            createItem({ content: inputVal });
-        }
-    }
 
     // TODO: move these to App for better coverage
     const handleClickEvent = () => {
@@ -57,23 +40,8 @@ export default function MainView() {
             position="bottom-center"
             autoClose={1000}
         />
-        <Group className="main-view__menu-flex-container">
-        </Group>
-        <Input id="main-input" className="main-view__input"
-            size="lg"
-            leftSection={<IconCircleTriangle
-                size={32}
-                stroke={justJotTheme.other.iconStrokeWidth}
-            />}
-            rightSectionPointerEvents="all"
-            rightSection={
-                isCreateItemLoading && <Loader size="xs"/>
-            }
-            type="text"
-            value={inputVal}
-            onChange={(event) => setInputVal(event.currentTarget.value)}
-            onKeyDown={handleKeyDown}
-        />
+
+        <MainInput/>
         <Stack className="main-view__items-container"
             gap="xs"
         >
