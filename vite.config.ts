e1ecedfc7852fa8069c54c48282c74884e588719
Juno Nguyen <juno.ngx@gmail.com>
2024-03-23 11:35:54 +0800

    Configure to import with @

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,8 @@ export default defineConfig({
     plugins: [react()],
     resolve: {
         alias: {
-           src: "/src"
+           src: "/src",
+           "@": "/src"
         },
      },
 })
