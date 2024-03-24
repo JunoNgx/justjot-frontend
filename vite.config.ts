18b752b6f6b37e9398e9fe047f45f53f89a54c65
Juno Nguyen <juno.ngx@gmail.com>
2024-03-24 21:19:12 +0800

    Configure for built-in mantine support for sass

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    css: {
+      preprocessorOptions: {
+         scss: {
+            additionalData: `@use "./src/_mantine";`,
+         }
+      }
+    },
     resolve: {
         alias: {
            "@": "/src"
