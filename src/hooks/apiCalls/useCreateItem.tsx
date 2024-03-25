79cf8d413dc3ad079c0b3966f5b408d4e8845776
Juno Nguyen <juno.ngx@gmail.com>
2024-03-25 20:51:37 +0800

    Implement createItemWithManipulation

--- src/hooks/apiCalls/useCreateItem.tsx
+++ src/hooks/apiCalls/useCreateItem.tsx
@@ -1,12 +1,15 @@
 import { useContext, useState } from "react";
 import { BackendClientContext } from "@/contexts/BackendClientContext";
-import { DbTable, RequestCallbackOptions, Item } from "@/types";
+import { DbTable, RequestCallbackOptions, Item, ItemType, ItemCreateUpdateType } from "@/types";
 import { notifications } from "@mantine/notifications";
 import { AUTO_CLOSE_ERROR_TOAST } from "@/utils/constants";
 import { CurrentCollectionContext } from "@/contexts/CurrentCollectionContext";
+import { ItemsContext } from "@/contexts/ItemsContext";
+import { DateTime } from "luxon";
 
 type useCreateItemReturnType = [
     ({ content }: { content: string }) => Promise<void>,
+    ({ title, content }: ItemCreateUpdateType) => void,
     boolean,
     boolean,
 ];
@@ -17,10 +20,11 @@ export default function useCreateItem(
 
     const { pbClient, user } = useContext(BackendClientContext);
     const { currCollection } = useContext(CurrentCollectionContext);
+    const { items, setItems } = useContext(ItemsContext);
     const [isSuccessful, setIsSuccessful] = useState(false);
     const [isLoading, setIsLoading] = useState(false);
 
-    const createItem = async ({ content }: { content: string }) => {
+    const createItem = async ({ content }: { content?: string }) => {
         setIsLoading(true);
         pbClient.collection(DbTable.ITEMS)
             .create({
@@ -45,8 +49,37 @@ export default function useCreateItem(
         setIsLoading(false);
     };
 
+    const generatePlaceholderItem = (
+        { title, content }: ItemCreateUpdateType
+    ): Item => {
+        const currDateTime = DateTime.now().toFormat("yyyy-MM-dd HH:mm:ss");
+        console.log(currDateTime)
+
+        return {
+            id: "placeholder",
+            owner: user?.id as string,
+            collection: currCollection?.id as string,
+            title: title as string,
+            content: content as string,
+            type: ItemType.TEXT as ItemType,
+            shouldCopyOnClick: false as boolean,
+            faviconUrl: "",
+            created: currDateTime,
+            updated: currDateTime,
+        };
+    };
+
+    const createItemWithManipulation = (
+        { title, content }: ItemCreateUpdateType
+    ) => {
+        const newItem = generatePlaceholderItem({title, content});
+        setItems([newItem, ...items || []]);
+        createItem({ content });
+    };
+
     return [
         createItem,
+        createItemWithManipulation,
         isLoading,
         isSuccessful,
     ];
