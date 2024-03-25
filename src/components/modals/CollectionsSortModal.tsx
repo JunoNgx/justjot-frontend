3f17cb7e8f5693159b2ff74d3c5ee5697df8b6da
Juno Nguyen <juno.ngx@gmail.com>
2024-03-25 21:24:33 +0800

    Clean up commented codes

--- src/components/modals/CollectionsSortModal.tsx
+++ src/components/modals/CollectionsSortModal.tsx
@@ -4,57 +4,12 @@ import { useListState } from "@mantine/hooks";
 import { useContext } from "react";
 import { CollectionsContext } from "@/contexts/CollectionsContext";
 
-// type CollectionsSortModalParam = {
-//     collectionList: ItemCollection[] | undefined
-// };
-
-// type DraggableCollection = ItemCollection & {
-//     index: number
-// };
-
-// type DraggableCollectionEvent = {
-//     source: DraggableCollection,
-//     destination: DraggableCollection,
-// };
-
-// const dummyCollection = [
-//     {
-//         id: "1",
-//         name: "1"
-//     },
-//     {
-//         id: "2",
-//         name: "2"
-//     },
-//     {
-//         id: "3",
-//         name: "3"
-//     },
-//     {
-//         id: "4",
-//         name: "4"
-//     },
-//     {
-//         id: "5",
-//         name: "5"
-//     },
-//     {
-//         id: "6",
-//         name: "6"
-//     },
-// ]
-
 export default function CollectionsSortModal() {
 
     const {collections} = useContext(CollectionsContext);
-    // const dummyCollection = collections.map(c => c);
-    // const [state, handlers] = useListState(dummyCollection);
     const [state, handlers] = useListState(collections);
 
     const handleDragEnd = ({destination, source}: DropResult) => {
-        console.log(source, destination)
-        // if (!destination) return;
-
         handlers.reorder({
             from: source.index,
             to: destination?.index || 0
@@ -63,11 +18,6 @@ export default function CollectionsSortModal() {
         // TODO: calculate new sort order
         // TODO: updateSortOrder
     };
-
-    // <Group className="collections-sort-modal__row-container">
-    // </Group>
-    //                     <CollectionHotkey index={index}/>
-
 
     const draggableItemList = (
         state.map((collection, index) => <Draggable
@@ -120,10 +70,8 @@ export default function CollectionsSortModal() {
     )
 
     return (
-
         <Stack className="collections-sort-modal" p="xl">
             {draggableArea}
         </Stack>
-
     )
 }
