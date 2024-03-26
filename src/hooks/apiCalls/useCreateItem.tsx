6eb7c3b17a7f76e9952259ddf803423aa686cad4
Juno Nguyen <juno.ngx@gmail.com>
2024-03-26 22:44:28 +0800

    Fix duplicated placeholder id again with unix in millisecs

--- src/hooks/apiCalls/useCreateItem.tsx
+++ src/hooks/apiCalls/useCreateItem.tsx
@@ -57,7 +57,7 @@ export default function useCreateItem(
         console.log(currDateTime)
 
         return {
-            id: DateTime.now().toUnixInteger().toString(),
+            id: new Date().getTime().toString(),
             owner: user?.id as string,
             collection: currCollection?.id as string,
             title: title as string,
