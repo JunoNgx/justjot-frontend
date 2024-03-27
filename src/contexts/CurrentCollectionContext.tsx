83561bccebed562ba291d3d3949236321bf21ad9
Juno Nguyen <juno.ngx@gmail.com>
2024-03-28 00:09:54 +0800

    Add currentSelectedCollectionIndexRef

--- src/contexts/CurrentCollectionContext.tsx
+++ src/contexts/CurrentCollectionContext.tsx
@@ -1,23 +1,27 @@
-import { ReactNode, createContext, useState } from 'react';
+import { ReactNode, createContext, useContext, useRef, useState } from 'react';
 import { ItemCollection } from '@/types';
 
 type CurrentCollectionContextType = {
     currCollection: ItemCollection | undefined,
     setCurrCollection: React.Dispatch<React.SetStateAction<ItemCollection | undefined>>,
+    currentSelectedCollectionIndexRef: React.MutableRefObject<number> | null,
 };
 
 export const CurrentCollectionContext = createContext<CurrentCollectionContextType>({
     currCollection: undefined,
     setCurrCollection: () => {},
+    currentSelectedCollectionIndexRef: null,
 });
 
 export default function CurrentCollectionContextProvider({ children }: { children: ReactNode }) {
     const [currCollection, setCurrCollection] = useState<ItemCollection>();
+    const currentSelectedCollectionIndexRef = useRef(0);
 
     return <CurrentCollectionContext.Provider value=
         {{
             currCollection,
             setCurrCollection,
+            currentSelectedCollectionIndexRef,
         }}
     >
         {children}
