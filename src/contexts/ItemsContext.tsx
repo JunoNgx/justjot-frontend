b4bdbc16d3f6a40a5e792ea1ff500c5e35117ffd
Juno Nguyen <juno.ngx@gmail.com>
2024-03-31 03:11:30 +0800

    Add update queue to items context

--- src/contexts/ItemsContext.tsx
+++ src/contexts/ItemsContext.tsx
@@ -1,11 +1,20 @@
-import { ReactNode, SetStateAction, createContext, useCallback, useContext, useState } from 'react';
+import { ReactNode, SetStateAction, createContext, useCallback, useContext, useEffect, useState } from 'react';
 import { DbTable, Item, ItemCollection } from '@/types';
 import { BackendClientContext } from '@/contexts/BackendClientContext';
+import { findIndexById } from '@/utils/itemUtils';
+import useManageListState from '@/libs/useManageListState';
+
+type UpdateQueueItem = {
+    tempId: string,
+    item: Item,
+};
 
 type ItemsContextType = {
     items: Item[],
     setItems: React.Dispatch<SetStateAction<Item[]>>,
     fetchItems: (currCollection: ItemCollection | undefined) => void,
+    updateQueue: UpdateQueueItem[],
+    setUpdateQueue: React.Dispatch<SetStateAction<UpdateQueueItem[]>>,
 };
 
 export const ItemsContext = createContext<ItemsContextType>(
@@ -15,6 +24,7 @@ export const ItemsContext = createContext<ItemsContextType>(
 export default function ItemsContextProvider({ children }: { children: ReactNode }) {
     const { pbClient } = useContext(BackendClientContext);
     const [ items, setItems ] = useState<Item[]>([]);
+    const [ updateQueue, setUpdateQueue ] = useState<UpdateQueueItem[]>([]);
 
     const fetchItems = useCallback(async (currCollection: ItemCollection | undefined) => {
         if (!currCollection) return;
@@ -39,11 +49,30 @@ export default function ItemsContextProvider({ children }: { children: ReactNode
     }, []);
 
 
+    const itemsHandlers = useManageListState(setItems);
+    const updateQueueHandlers = useManageListState(setUpdateQueue);
+
+    /**
+     * Attempt to track down temporary items, and replace them with
+     * appropriate correct data
+     */
+    useEffect(() => {
+        updateQueue.forEach((updateQueueItem, updateQueueIndex) => {
+            const index = findIndexById(updateQueueItem.tempId, items);
+            if (index === -1) return;
+
+            itemsHandlers.replace(index, updateQueueItem.item);
+            updateQueueHandlers.remove(updateQueueIndex);
+        });
+    }, [items, updateQueue])
+
     return <ItemsContext.Provider value=
         {{
             items,
             setItems,
-            fetchItems
+            fetchItems,
+            updateQueue,
+            setUpdateQueue,
         }}
     >
         {children}
