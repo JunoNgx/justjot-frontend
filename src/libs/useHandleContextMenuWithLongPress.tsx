0f347a8dc16aec748697a4f49aaec98abcdebd99
Juno Nguyen <juno.ngx@gmail.com>
2024-03-30 12:32:47 +0800

    Clean up comments and logic

--- src/libs/useHandleContextMenuWithLongPress.tsx
+++ src/libs/useHandleContextMenuWithLongPress.tsx
@@ -3,98 +3,39 @@
 
 import { useCallback, useRef } from "react";
 
-// function preventDefault(e: Event) {
-//     if (!isTouchEvent(e)) return;
-
-//     if (e.touches.length < 2 && e.preventDefault) {
-//         e.preventDefault();
-//     }
-// };
-
-// export function isTouchEvent(e: Event): e is TouchEvent {
-//     return e && "touches" in e;
-// };
-
 interface PressHandlers {
-    onLongPress: (e: React.MouseEvent | React.TouchEvent) => void,
     onClick: (e: React.MouseEvent | React.TouchEvent) => void,
-}
+    onLongPress: (e: React.MouseEvent | React.TouchEvent) => void,
+};
 
 interface Options {
     delay?: number,
-    // shouldPreventDefault?: boolean
-}
+};
 
 export default function useHandleContextMenuWithLongPress(
     { onClick, onLongPress }: PressHandlers,
     { delay = 300 } : Options = {},
 ) {
-    // const [longPressTriggered, setLongPressTriggered] = useState(false);
     const timeout = useRef<number>();
-    // const target = useRef<EventTarget>();
 
-    const start = useCallback(
-        (e: React.MouseEvent | React.TouchEvent) => {
-            // console.log("start")
-            /**
-             * Juno's note:
-             * Remove the use of `e.persist()` and the use of cloned events
-             * for two reasons:
-             * - This is no-op since React v17
-             * - `mantine-contextmenu` needs to `preventDefault` on its own.
-             * Preferrably: I should fork it and add an optional chaining opterator.
-             */
-            // e.persist();
-            // const clonedEvent = { ...e };
-
-            // if (shouldPreventDefault && e.target) {
-            //     e.target.addEventListener(
-            //         "touchend",
-            //         preventDefault,
-            //         { passive: false }
-            //     );
-            //     target.current = e.target;
-            // }
-
-            // Prevent right mouse click/contextmenu interference
-            if (("button" in e) && e.button === 2) {
-                return;
-            }
-
-            timeout.current = setTimeout(() => {
-                // clear(e, false);
-                // onLongPress(e);
-                handleContextMenu(e)
-                // setLongPressTriggered(true);
-            }, delay);
-        },
-        // [onLongPress, delay, shouldPreventDefault]
-        [onLongPress, delay]
-    );
-
-    const clear = useCallback((
-        e: React.MouseEvent | React.TouchEvent,
-        // shouldTriggerClick = true
+    const start = useCallback((
+        e: React.MouseEvent | React.TouchEvent
     ) => {
-        // console.log("clear", shouldTriggerClick)
-        timeout.current && clearTimeout(timeout.current);
-
-        // Prevent right mouse click/contextmenu interference
+        // Prevent right mouse click/native contextmenu interference
         if (("button" in e) && e.button === 2) {
             return;
         }
 
-        // shouldTriggerClick && !longPressTriggered && onClick?.(e);
+        timeout.current = setTimeout(() => {
+            handleContextMenu(e)
+        }, delay);
+    }, [onLongPress, delay]);
 
-        // setLongPressTriggered(false);
-
-        // if (shouldPreventDefault && target.current) {
-        //     target.current.removeEventListener("touchend", preventDefault);
-        // }
-    },
-        // [shouldPreventDefault, onClick, longPressTriggered]
-        [onClick]
-    );
+    const clear = useCallback((
+        _e: React.MouseEvent | React.TouchEvent,
+    ) => {
+        timeout.current && clearTimeout(timeout.current);
+    }, [onClick]);
 
     const handleContextMenu = (
         e: React.MouseEvent | React.TouchEvent
