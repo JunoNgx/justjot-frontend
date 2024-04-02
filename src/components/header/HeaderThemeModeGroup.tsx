d74b9fba8d5dd59a13d05a455671bd956f1006ad
Juno Nguyen <juno.ngx@gmail.com>
2024-04-03 00:45:29 +0800

    Revamp theme mode buttons

--- src/components/header/HeaderThemeModeGroup.tsx
+++ src/components/header/HeaderThemeModeGroup.tsx
@@ -1,32 +1,28 @@
 import { useContext } from "react";
 
-import { ActionIcon, Group, useComputedColorScheme } from "@mantine/core";
+import { ActionIcon, Group } from "@mantine/core";
 import { ThemeModeContext } from "@/contexts/ThemeModeContext";
-import { ThemeMode, ComputedThemeMode } from "@/types";
+import { ThemeMode } from "@/types";
 import { IconMoon, IconSettingsCog, IconSun } from "@tabler/icons-react";
 import { justJotTheme } from "@/theme";
 import useIconPropsFromTheme from "@/hooks/useIconPropsFromTheme";
 
 export default function HeaderThemeModeGroup() {
     const { themeMode, setThemeMode } = useContext(ThemeModeContext);
-    const computedThemeMode = useComputedColorScheme(ComputedThemeMode.LIGHT);
-
     const iconProps = useIconPropsFromTheme();
 
     const isThemeModeLight = () => themeMode === ThemeMode.LIGHT;
     const isThemeModeDark = () => themeMode === ThemeMode.DARK;
     const isThemeModeAuto = () => themeMode === ThemeMode.AUTO;
 
-    const themeModeBtnColour = computedThemeMode === ComputedThemeMode.LIGHT
-        ? justJotTheme.other.colText
-        : justJotTheme.other.colTextDark;
-
     return <Group className="header__theme-mode-container"
         gap="xs"
     >
-        <ActionIcon className="header__theme-mode-btn"
-            variant={isThemeModeAuto() ? "outline" : "subtle"}
-            color={themeModeBtnColour}
+        <ActionIcon
+            className={"header__theme-mode-btn "
+                + (isThemeModeAuto() ? "header__theme-mode-btn--is-selected" : null)
+            }
+            variant={"subtle"}
             radius="xl"
             onClick={() => {setThemeMode(ThemeMode.AUTO)}}
         >
@@ -35,9 +31,11 @@ export default function HeaderThemeModeGroup() {
                 size={justJotTheme.other.iconSizeThemeMode}
             />
         </ActionIcon>
-        <ActionIcon className="header__theme-mode-btn"
-            variant={isThemeModeLight() ? "outline" : "subtle"}
-            color={themeModeBtnColour}
+        <ActionIcon
+            className={"header__theme-mode-btn "
+                + (isThemeModeLight() ? "header__theme-mode-btn--is-selected" : null)
+            }
+            variant={"subtle"}
             radius="xl"
             onClick={() => {setThemeMode(ThemeMode.LIGHT)}}
         >
@@ -46,9 +44,11 @@ export default function HeaderThemeModeGroup() {
                 size={justJotTheme.other.iconSizeThemeMode}
             />
         </ActionIcon>
-        <ActionIcon className="header__theme-mode-btn"
-            variant={isThemeModeDark() ? "outline" : "subtle"}
-            color={themeModeBtnColour}
+        <ActionIcon
+            className={"header__theme-mode-btn "
+                + (isThemeModeDark() ? "header__theme-mode-btn--is-selected" : null)
+            }
+            variant={"subtle"}
             radius="xl"
             onClick={() => {setThemeMode(ThemeMode.DARK)}}
         >
