a092de947b789b957ed926f7b1b0041842e76186
Juno Nguyen <juno.ngx@gmail.com>
2024-04-03 01:46:12 +0800

    Add convertToTodoFn to item context menu

--- src/hooks/useItemContextMenuOptions.tsx
+++ src/hooks/useItemContextMenuOptions.tsx
@@ -1,6 +1,6 @@
 import { CollectionsContext } from "@/contexts/CollectionsContext";
 import { Item, ItemCollection, ItemType } from "@/types";
-import { IconCheckbox, IconCopy, IconDownload, IconEdit, IconFileSymlink, IconSquare, IconTrash } from "@tabler/icons-react";
+import { IconArrowMoveRight, IconCheckbox, IconCopy, IconDownload, IconEdit, IconFileSymlink, IconSquare, IconTrash } from "@tabler/icons-react";
 import { useContext } from "react";
 import useIconPropsFromTheme from "./useIconPropsFromTheme";
 
@@ -13,11 +13,21 @@ type ItemContextMenuOptionsParams = {
     refetchFn: (item: Item) => void,
     toggleCopyFn: ({item}: {item: Item}) => void,
     deselectFn: () => void,
+    convertToTodoFn: ({item}: {item: Item}) => void,
 };
 
 export default function useItemContextMenuOptions(
-    { item, copyFn, editFn, moveFn, deleteFn, refetchFn, toggleCopyFn, deselectFn }:
-    ItemContextMenuOptionsParams
+    {
+        item,
+        copyFn,
+        editFn,
+        moveFn,
+        deleteFn,
+        refetchFn,
+        toggleCopyFn,
+        deselectFn,
+        convertToTodoFn,
+    }: ItemContextMenuOptionsParams
 ) {
     const { collections } = useContext(CollectionsContext);
     const iconProps = useIconPropsFromTheme();
@@ -53,6 +63,13 @@ export default function useItemContextMenuOptions(
             hidden: item.type !== ItemType.LINK,
             icon: <IconDownload {...iconProps} />,
             onClick: () => {refetchFn(item);}
+        }, {
+            key: "convertToTodo",
+            title: "Convert to Todo",
+            hidden: item.title || !item.content || item.type !== ItemType.TEXT,
+            icon: <IconArrowMoveRight {...iconProps} />,
+            color: "orange",
+            onClick: () => {convertToTodoFn({item});}
         }, {
             key: "divider",
         }, {
