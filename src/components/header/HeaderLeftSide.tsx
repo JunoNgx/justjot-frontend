1b679b131282166bf9e8e603e3f0a8cf51a34f73
Juno Nguyen <juno.ngx@gmail.com>
2024-04-03 00:47:30 +0800

    Use css for logo colour

--- src/components/header/HeaderLeftSide.tsx
+++ src/components/header/HeaderLeftSide.tsx
@@ -1,20 +1,14 @@
-import { ActionIcon, Group, useComputedColorScheme } from "@mantine/core";
-import { justJotTheme } from "@/theme";
+import { ActionIcon, Group } from "@mantine/core";
 import { NavLink } from "react-router-dom";
 import CollectionMenu from "@/components/CollectionMenu";
 import { useContext } from "react";
 import { BackendClientContext } from "@/contexts/BackendClientContext";
 import { JustJotIcon } from "@/components/misc/JustJotIcon";
-import { ComputedThemeMode } from "@/types";
 import useIconPropsFromTheme from "@/hooks/useIconPropsFromTheme";
 
 export default function HeaderLeftSide() {
 
     const { user } = useContext(BackendClientContext);
-    const computedThemeMode = useComputedColorScheme(ComputedThemeMode.LIGHT);
-    const themeModeBtnColour = computedThemeMode === ComputedThemeMode.LIGHT
-        ? justJotTheme.other.colText
-        : justJotTheme.other.colTextDark;
     const iconProps = useIconPropsFromTheme();
     
     return <Group className="header__left-side"
@@ -26,7 +20,9 @@ export default function HeaderLeftSide() {
             to={user ? `/${user.username}` : "/"}
             size="xl"
         >
-            <JustJotIcon {...iconProps} color={themeModeBtnColour} />
+            <JustJotIcon className="header__logo"
+                {...iconProps}
+            />
         </ActionIcon>
         <CollectionMenu/>
     </Group>
