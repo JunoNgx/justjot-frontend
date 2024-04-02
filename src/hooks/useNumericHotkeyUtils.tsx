eb11b3306de76cb50d1959ddf2d5a930c91c277a
Juno Nguyen <juno.ngx@gmail.com>
2024-04-02 20:40:47 +0800

    Implement and rename to useNumericHotkeyUtils

---
+++ src/hooks/useNumericHotkeyUtils.tsx
@@ -0,0 +1,33 @@
+import { HotkeyItem } from "@mantine/hooks";
+
+export default function useNumericHotkeyUtils() {
+
+    const computeIndexFromNumericKey = (inputNumber: number): number => {
+        switch (true) {
+        case (inputNumber < 0 || inputNumber > 9):
+            return -1;
+        case (inputNumber === 0):
+            return 9;
+        default:
+            return inputNumber;
+        }
+    };
+
+    const useGenerateNumericHotkeyOptions = (
+        {callback}: {callback: (n: number) => void}
+    ): HotkeyItem[] => {
+        return Array.from(Array(9).keys())
+            .map(n => [
+                n.toString(),
+                () => { callback(n)}
+            ])
+    };
+
+    return {
+        computeIndexFromNumericKey,
+        useGenerateNumericHotkeyOptions,
+    };
+};
+
+
+
