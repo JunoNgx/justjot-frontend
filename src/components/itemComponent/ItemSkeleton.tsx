081fb8a414972318e54e08c32e196edd0f1f9d50
Juno Nguyen <juno.ngx@gmail.com>
2024-04-04 21:49:49 +0800

    Implement ItemSkeleton

---
+++ src/components/itemComponent/ItemSkeleton.tsx
@@ -0,0 +1,21 @@
+import { Group, Skeleton } from "@mantine/core";
+
+export default function ItemSkeleton() {
+    return <Group className="item-skeleton"
+        justify="space-between"
+    >
+        <Group className="item-skeleton__left-side">
+            <Skeleton className="item-skeleton__icon-wrapper"
+                width={32} height={32} 
+            />
+            <Skeleton className="item-skeleton__primary-text"
+                height={32}
+            />
+        </Group>
+        <Group className="item-skeleton__right-side">
+            <Skeleton className="item-skeleton__datetime"
+                height={32} width="5rem"
+            />
+        </Group>
+    </Group>
+};
