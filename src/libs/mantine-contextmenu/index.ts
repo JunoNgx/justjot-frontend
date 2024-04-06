cbc230d62132ddd262ffe5d8515189ffca1b3bcf
Juno Nguyen <juno.ngx@gmail.com>
2024-04-06 15:20:53 +0800

    Remove existing forked version of mantine-contextmenu

--- src/libs/mantine-contextmenu/index.ts
+++
@@ -1,8 +0,0 @@
-'use client';
-
-export * from './ContextMenuDivider';
-export * from './ContextMenuItem';
-export * from './ContextMenuPortal';
-export * from './ContextMenuProvider';
-export * from './types';
-export * from './utils';
