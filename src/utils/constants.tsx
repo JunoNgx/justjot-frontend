a72a46d8fee8234cf29720c4256810d9700595ee
Juno Nguyen <juno.ngx@gmail.com>
2024-04-06 09:52:21 +0800

    Rearrange constants

--- src/utils/constants.tsx
+++ src/utils/constants.tsx
@@ -1,8 +1,8 @@
+export const APP_NAME = "JustJot";
 export const SORT_ORDER_INCREMENT_COLLECTION = 1000;
 export const SORT_ORDER_INCREMENT_TODO_ITEM = 1000;
 export const AUTO_CLOSE_ERROR_TOAST = false;
 export const AUTO_CLOSE_DEFAULT = 3000;
 export const COLLECTION_SORT_ORDER_MAG = 1000;
-export const APP_NAME = "JustJot";
 export const MAX_TITLE_LENGTH = 200;
 export const MAX_CONTENT_LENGTH = 10000;
