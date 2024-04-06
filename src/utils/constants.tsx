07742961315562466a4d29ca2c558e8295c1c95b
Juno Nguyen <juno.ngx@gmail.com>
2024-04-06 13:11:06 +0800

    Add CREATE_TODO_PREFIX

--- src/utils/constants.tsx
+++ src/utils/constants.tsx
@@ -8,3 +8,4 @@ export const MAX_TITLE_LENGTH = 200;
 export const MAX_CONTENT_LENGTH = 10000;
 export const CREATE_TEXT_WITH_TITLE_PREFIX = ":t:";
 export const CREATE_TEXT_WITH_TITLE_PREFIX_ALT = ":l:";
+export const CREATE_TODO_PREFIX = ":td:";
