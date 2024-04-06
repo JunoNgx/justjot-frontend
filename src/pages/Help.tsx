311f50ebe13ae5cf03dfbd8fc265f56a645765f1
Juno Nguyen <juno.ngx@gmail.com>
2024-04-06 15:50:05 +0800

    Implement deselecting collection when navigating to non-collection route

--- src/pages/Help.tsx
+++ src/pages/Help.tsx
@@ -1,3 +1,12 @@
+import { CurrentCollectionContext } from "@/contexts/CurrentCollectionContext";
+import { useContext, useEffect } from "react";
+
 export default function Help() {
+    const { setCurrCollection } = useContext(CurrentCollectionContext);
+
+    useEffect(() => {
+        setCurrCollection(undefined);
+    });
+
     return <h1>Help route</h1>
 }
--- src/pages/UserProfile.tsx
+++ src/pages/UserProfile.tsx
@@ -1,5 +1,14 @@
+import { CurrentCollectionContext } from "@/contexts/CurrentCollectionContext";
 import { Text } from "@mantine/core";
+import { useContext, useEffect } from "react";
 
 export default function UserProfile() {
+
+    const { setCurrCollection } = useContext(CurrentCollectionContext);
+
+    useEffect(() => {
+        setCurrCollection(undefined);
+    });
+
     return <Text>User profile</Text>
 };
