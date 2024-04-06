0957cf726e90b76b6ad54ff52fcc8d18c9f2e006
Juno Nguyen <juno.ngx@gmail.com>
2024-04-06 13:11:58 +0800

    Implement prepending prefixes from extended menu

--- src/components/MainInputExtendedMenu.tsx
+++ src/components/MainInputExtendedMenu.tsx
@@ -1,10 +1,14 @@
+import { ItemsContext } from "@/contexts/ItemsContext";
 import useIconPropsFromTheme from "@/hooks/useIconPropsFromTheme";
+import { CREATE_TEXT_WITH_TITLE_PREFIX, CREATE_TODO_PREFIX } from "@/utils/constants";
 import { Box, Menu } from "@mantine/core";
 import { IconCheckbox, IconChevronDown, IconClipboardPlus, IconLayoutNavbar } from "@tabler/icons-react";
+import { useContext } from "react";
 
 export default function MainInputExtendedMenu() {
 
     const iconProps = useIconPropsFromTheme();
+    const { setInputVal } = useContext(ItemsContext);
 
     return <Menu 
         position="bottom-end"
@@ -23,11 +27,13 @@ export default function MainInputExtendedMenu() {
         <Menu.Dropdown className="dropdown-menu">
             <Menu.Item
                 leftSection={<IconLayoutNavbar {...iconProps} />}
+                onClick={() => setInputVal(curr => `${CREATE_TEXT_WITH_TITLE_PREFIX} ${curr}`)}
             >
                 with title
             </Menu.Item>
             <Menu.Item
                 leftSection={<IconCheckbox {...iconProps} />}
+                onClick={() => setInputVal(curr => `${CREATE_TODO_PREFIX} ${curr}`)}
             >
                 as todo
             </Menu.Item>
