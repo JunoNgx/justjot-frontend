9b1809b67c43bb1c7e99c9aa1837b2bbf78832b9
Juno Nguyen <juno.ngx@gmail.com>
2024-04-06 18:16:47 +0800

    Logout from user profile page if not logged in

--- src/pages/UserProfile.tsx
+++ src/pages/UserProfile.tsx
@@ -1,21 +1,35 @@
 import ProfileChangeDisplayName from "@/components/profile/ProfileChangeDisplayName";
 import ProfileChangeEmail from "@/components/profile/ProfileChangeEmail";
 import ProfileChangePassword from "@/components/profile/ProfileChangePassword";
+import { BackendClientContext } from "@/contexts/BackendClientContext";
 import { CurrentCollectionContext } from "@/contexts/CurrentCollectionContext";
 import { Box } from "@mantine/core";
 import { useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function UserProfile() {
 
+    const { isLoggedIn } = useContext(BackendClientContext);
     const { setCurrCollection } = useContext(CurrentCollectionContext);
 
+    const navigate = useNavigate();
+
     useEffect(() => {
+        if (!isLoggedIn) {
+            navigate(`/login`, { replace: true });
+            return;
+        }
+
         setCurrCollection(undefined);
-    });
+    }, []);
 
     return <Box>
-        <ProfileChangeDisplayName/>
-        <ProfileChangePassword/>
-        <ProfileChangeEmail/>
+        {isLoggedIn &&
+            <>
+                <ProfileChangeDisplayName/>
+                <ProfileChangePassword/>
+                <ProfileChangeEmail/>
+            </>
+        }
     </Box>
 };
