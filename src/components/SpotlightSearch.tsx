a13c9af4f40b6007fdfe51ea1c583c52a4013e88
Juno Nguyen <juno.ngx@gmail.com>
2024-04-07 14:20:55 +0800

    Sort spotlight actions by group

--- src/components/SpotlightSearch.tsx
+++ src/components/SpotlightSearch.tsx
@@ -1,7 +1,7 @@
 import { ThemeModeContext } from '@/contexts/ThemeModeContext';
 import useIconPropsFromTheme from '@/hooks/useIconPropsFromTheme';
 import { ThemeMode } from '@/types';
-import { Spotlight, SpotlightActionData } from '@mantine/spotlight';
+import { Spotlight, SpotlightActionData, SpotlightActionGroupData } from '@mantine/spotlight';
 import { IconMoon, IconSettingsCog, IconSun } from '@tabler/icons-react';
 import { useContext } from 'react';
 
@@ -9,30 +9,32 @@ export default function SpotlightSearch() {
     const { setThemeMode } = useContext(ThemeModeContext);
     const iconProps = useIconPropsFromTheme();
 
-
-    const themeModeActions: SpotlightActionData[] = [
-        {
-            id: "theme-mode-system",
-            label: "Theme mode: system",
-            leftSection: <IconSettingsCog {...iconProps} />,
-            onClick: () => {setThemeMode(ThemeMode.AUTO)},
-        },
-        {
-            id: "theme-mode-light",
-            label: "Theme mode: light",
-            leftSection: <IconSun {...iconProps} />,
-            onClick: () => {setThemeMode(ThemeMode.LIGHT)}
-        },
-        {
-            id: "theme-mode-system",
-            label: "Theme mode: dark",
-            leftSection: <IconMoon {...iconProps} />,
-            onClick: () => {setThemeMode(ThemeMode.DARK)}
-        },
-    ];
-
-    const actions: SpotlightActionData[] = [
-        ...themeModeActions
+    const themeModeActionGroup: SpotlightActionGroupData = {
+        group: "Theme modes",
+        actions: [
+            {
+                id: "theme-mode-system",
+                label: "Theme mode: system",
+                leftSection: <IconSettingsCog {...iconProps} />,
+                onClick: () => {setThemeMode(ThemeMode.AUTO)},
+            },
+            {
+                id: "theme-mode-light",
+                label: "Theme mode: light",
+                leftSection: <IconSun {...iconProps} />,
+                onClick: () => {setThemeMode(ThemeMode.LIGHT)}
+            },
+            {
+                id: "theme-mode-system",
+                label: "Theme mode: dark",
+                leftSection: <IconMoon {...iconProps} />,
+                onClick: () => {setThemeMode(ThemeMode.DARK)}
+            },
+        ]
+    };
+        
+    const actions: (SpotlightActionGroupData | SpotlightActionData)[] = [
+        themeModeActionGroup
     ];
 
     return <Spotlight
