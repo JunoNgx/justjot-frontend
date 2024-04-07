07bfe977c2ff02724a03a2150b956764d5365279
Juno Nguyen <juno.ngx@gmail.com>
2024-04-07 15:05:09 +0800

    Clean up useIconPropsFromTheme

--- src/hooks/useIconPropsFromTheme.tsx
+++ src/hooks/useIconPropsFromTheme.tsx
@@ -3,28 +3,28 @@ export default function useIconPropsFromTheme() {
     const itemIcontProps = {
         size: 24,
         stroke: 1,
-    }
+    };
 
     const themeModeIconProps = {
         size: 18,
         stroke: 1.5,
-    }
+    };
+
+    const menuIconProps = {
+        size: 18,
+        stroke: 1.5,
+    };
+
+    const mainInputIconProps = {
+        size: 32,
+        stroke: 1,
+    };
 
     const logoIconProps = {
         size: 24,
         // TODO: figure why the custom icon is acting up
         stroke: "1.5",
-    }
-
-    const menuIconProps = {
-        size: 18,
-        stroke: 1.5,
-    }
-
-    const mainInputIconProps = {
-        size: 32,
-        stroke: 1,
-    }
+    };
 
     return {
         itemIcontProps,
