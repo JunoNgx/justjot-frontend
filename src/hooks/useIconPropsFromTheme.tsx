157e24cf61d1444ae81dfbc140d570b64ad9a31f
Juno Nguyen <juno.ngx@gmail.com>
2024-04-07 14:34:11 +0800

    Revamp useIconPropsFromTheme without using theme

--- src/hooks/useIconPropsFromTheme.tsx
+++ src/hooks/useIconPropsFromTheme.tsx
@@ -1,15 +1,13 @@
-import { justJotTheme } from "@/theme";
-
 export default function useIconPropsFromTheme() {
 
     const itemIcontProps = {
-        size: justJotTheme.other.iconSizeItem,
-        stroke: justJotTheme.other.iconStrokeWidth,
+        size: 24,
+        stroke: 1,
     }
 
     const themeModeIconProps = {
-        size: justJotTheme.other.iconSizeThemeMode,
-        stroke: justJotTheme.other.iconStrokeWidth,
+        size: 18,
+        stroke: 1.5,
     }
 
     return {
