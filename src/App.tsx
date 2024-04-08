c293b22d8abfa5ecfbe4a6562587ccf0fc82e435
Juno Nguyen <juno.ngx@gmail.com>
2024-04-09 00:33:40 +0800

    Add OverlayScrollbarsComponent

--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,11 @@ import '@mantine/spotlight/styles.css';
 import '@mantine/core/styles.layer.css';
 
 import { ContextMenuProvider } from 'mantine-contextmenu';
+import { OverlayScrollbarsComponent } from 'overlayscrollbars-react';
 import "mantine-contextmenu/styles.css";
 import 'mantine-contextmenu/styles.layer.css';
 import '@/styles/layers.css';
+import 'overlayscrollbars/styles/overlayscrollbars.css';
 
 import ThemeModeContextProvider from "@/contexts/ThemeModeContext";
 import BackendClientContextProvider from "@/contexts/BackendClientContext";
@@ -33,60 +35,76 @@ import SpotlightSearch from "./components/SpotlightSearch";
 
 function App() {
 
-    return <AppShell
-        header={{ height: 45}}
-        padding="none"
-    >
-        <ThemeModeContextProvider>
+    return (
+        <AppShell
+            header={{ height: 45}}
+            padding="none"
+        >
+            <ThemeModeContextProvider>
 
-            <BackendClientContextProvider>
-                <CollectionsContextProvider>
-                    <ItemsContextProvider>
+                <BackendClientContextProvider>
+                    <CollectionsContextProvider>
+                        <ItemsContextProvider>
+                            <ModalsProvider
+                                modals={{
+                                    infoModal: InfoModal,
+                                    itemCreateModal: ItemCreateModal
+                                }}
+                            >
+                                <ContextMenuProvider>
 
-                        <ModalsProvider
-                            modals={{
-                                infoModal: InfoModal,
-                                itemCreateModal: ItemCreateModal
-                            }}
-                        >
-                            <ContextMenuProvider>
+                                    <OverlayScrollbarsComponent
+                                        className="overlayScrollbar"
+                                        options={{
+                                            scrollbars: {
+                                                theme: "os-theme-dark",
+                                                visibility: 'auto',
+                                                autoHide: 'scroll',
+                                                autoHideDelay: 1000,
+                                            },
+                                        }}
+                                        defer
+                                    >
 
-                                <Notifications
-                                    limit={5}
-                                    position="bottom-center"
-                                    autoClose={1000}
-                                />
+                                        <Notifications
+                                            limit={5}
+                                            position="bottom-center"
+                                            autoClose={1000}
+                                        />
 
-                                <AppShell.Header>
-                                    <Header/>
-                                </AppShell.Header>
+                                        <AppShell.Header>
+                                            <Header/>
+                                        </AppShell.Header>
 
-                                <AppShell.Main>
-                                    <Routes>
-                                        <Route path="/" element={<LandingPage />} />
-                                        <Route path="/help" element={<Help />} />
-                                        <Route path="/:username">
-                                            <Route index element={<MainView />} />
-                                            <Route path=":collectionSlug" element={<MainView />} />
-                                        </Route>
-                                        <Route path="/profile" element={<Profile />} />
-                                        <Route path="/login" element={<Login />} />
-                                        <Route path="/demo-login" element={<Login isDemoMode={true}/>} />
-                                        <Route path="/register" element={<Register />} />
-                                        <Route path="/reset" element={<Reset />} />
-                                    </Routes>
-                                    <SpotlightSearch/>
-                                </AppShell.Main>
+                                        <AppShell.Main>
+                                            <Routes>
+                                                <Route path="/" element={<LandingPage />} />
+                                                <Route path="/help" element={<Help />} />
+                                                <Route path="/:username">
+                                                    <Route index element={<MainView />} />
+                                                    <Route path=":collectionSlug" element={<MainView />} />
+                                                </Route>
+                                                <Route path="/profile" element={<Profile />} />
+                                                <Route path="/login" element={<Login />} />
+                                                <Route path="/demo-login" element={<Login isDemoMode={true}/>} />
+                                                <Route path="/register" element={<Register />} />
+                                                <Route path="/reset" element={<Reset />} />
+                                            </Routes>
+                                            <SpotlightSearch/>
 
-                            </ContextMenuProvider>
-                        </ModalsProvider>
+                                        </AppShell.Main>
+                                    </OverlayScrollbarsComponent>
 
-                    </ItemsContextProvider>
-                </CollectionsContextProvider>
-            </BackendClientContextProvider>
+                                </ContextMenuProvider>
+                            </ModalsProvider>
+                        </ItemsContextProvider>
+                    </CollectionsContextProvider>
+                </BackendClientContextProvider>
 
-        </ThemeModeContextProvider>
-    </AppShell>
+            </ThemeModeContextProvider>
+
+        </AppShell>
+    )
 }
 
 export default App;
