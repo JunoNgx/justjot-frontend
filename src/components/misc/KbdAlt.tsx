d83d9e2e2c75dfe96881f6007da58b847d68a0c5
Juno Nguyen <juno.ngx@gmail.com>
2024-04-11 22:35:15 +0800

    Implement KbdAlt

---
+++ src/components/misc/KbdAlt.tsx
@@ -0,0 +1,14 @@
+import { Kbd } from "@mantine/core";
+import { useOs } from "@mantine/hooks";
+
+export default function KbdAlt() {
+    const os = useOs();
+
+    switch(true) {
+    case (os === "macos"):
+    case (os === "ios"):
+        return <Kbd>Opt</Kbd>;
+    default:
+        return <Kbd>Alt</Kbd>;
+    }
+};
