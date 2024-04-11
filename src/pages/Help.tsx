b6cc4c8f44414c54664f56ccd77daba6867f58c1
Juno Nguyen <juno.ngx@gmail.com>
2024-04-12 02:17:10 +0800

    Add special syntax note for todo

--- src/pages/Help.tsx
+++ src/pages/Help.tsx
@@ -85,7 +85,7 @@ export default function Help() {
             <Text>If your entered input is detected as a url, it will be stored as a link. Links also contain metadata (page title and favicon), and attempts are always made to retrieve them for links.</Text>
 
             <Title order={4}>Todo</Title>
-            <Text>A todo item can be marked as completed. A text item without title can be converted into a todo item (though this can be a semi-destructive operation, due the title length limit, and is not reversible).</Text>
+            <Text>A todo item can be marked as completed. A text item without title can be converted into a todo item (though this can be a semi-destructive operation, due the title length limit, and is not reversible). To quickly create a todo action from the Main Input, use the <code>:td:</code> prefix.</Text>
 
             <Title order={4}>Hex colour code</Title>
             <Text>As a special subtype of note, a hex colour code, when detected, will have itself displayed as the icon. The logic validates the last seven characters; hence as a lifehack, prepend a hex colour code to your text note to mark it with a coloured icon.</Text>
