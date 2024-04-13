1dd26217e1ba269c5df97ee1173848095c7784a1
Juno Nguyen <juno.ngx@gmail.com>
2024-04-13 13:44:30 +0800

    Add isFaviconEnabled to ThemeModeContext

--- src/contexts/ThemeModeContext.tsx
+++ src/contexts/ThemeModeContext.tsx
@@ -6,18 +6,21 @@ import { useLocalStorage } from "@mantine/hooks";
 
 type ThemeModeContextType = {
     themeMode: ThemeMode,
-    setThemeMode: React.Dispatch<React.SetStateAction<ThemeMode>>
+    setThemeMode: React.Dispatch<React.SetStateAction<ThemeMode>>,
+    isFaviconEnabled: boolean,
+    setIsFaviconEnabled: React.Dispatch<React.SetStateAction<boolean>>,
 }
 
-export const ThemeModeContext = createContext<ThemeModeContextType>({
-    themeMode: ThemeMode.AUTO,
-    setThemeMode: () => {}
-});
+export const ThemeModeContext = createContext<ThemeModeContextType>({} as ThemeModeContextType);
 
 export default function ThemeModeContextProvider({children}: {children: ReactNode}) {
     const [themeMode, setThemeMode] = useLocalStorage<ThemeMode>({
         key: "color-scheme",
         defaultValue: ThemeMode.AUTO,
+    });
+    const [isFaviconEnabled, setIsFaviconEnabled] = useLocalStorage<boolean>({
+        key: "isFaviconEnabled",
+        defaultValue: true,
     });
 
     const {
@@ -29,7 +32,12 @@ export default function ThemeModeContextProvider({children}: {children: ReactNod
         setMantineColorScheme(themeMode);
     }, [themeMode]);
 
-    return <ThemeModeContext.Provider value={ { themeMode, setThemeMode } }>
+    return <ThemeModeContext.Provider value={{
+        themeMode,
+        setThemeMode,
+        isFaviconEnabled,
+        setIsFaviconEnabled,
+    }}>
         {children}
     </ThemeModeContext.Provider>
 }
