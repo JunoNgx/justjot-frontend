97c2702aa9f5d72dc07ff8d54e1dc5a269520472
Juno Nguyen <juno.ngx@gmail.com>
2024-04-13 21:43:02 +0800

    Implement ProfileTestAccountNotice

---
+++ src/components/profile/ProfileTestAccountNotice.tsx
@@ -0,0 +1,16 @@
+import { Paper, Text, Title } from "@mantine/core";
+
+export default function ProfileTestAccountNotice() {
+    return <Paper className="cardlike"
+        withBorder
+        p="md"
+    >
+        <Title className="cardlike__title"
+            order={2}
+        >
+            Test account notice
+        </Title>
+        
+        <Text>Several functionalities are limited and/or not displayed for the test account.</Text>
+    </Paper>
+};
