b32b8673e660ec2b4f592af1fec21cbd1767510e
Juno Nguyen <juno.ngx@gmail.com>
2024-04-20 14:13:16 +0800

    Remove unused util function

--- src/utils/miscUtils.tsx
+++ src/utils/miscUtils.tsx
@@ -1,13 +1,3 @@
-export const isDatetimeMoreThanOneYearOld = (datetime: Date): boolean => {
-    const now = new Date();
-
-    const yearDiff = now.getFullYear() - datetime.getFullYear();
-    const dateDiff = now.setFullYear(2000) - datetime.setFullYear(2000);
-
-    return yearDiff > 1
-        || yearDiff === 1 && dateDiff > 0;
-}
-
 export const isValidUrl = (url: string) => {
     try { 
         return Boolean(new URL(url)); 
