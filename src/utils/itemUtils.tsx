66c78c84c4b2d895fc090ec97fe5b2f5cee86994
Juno Nguyen <juno.ngx@gmail.com>
2024-04-20 14:12:38 +0800

    Clean up commented codes

--- src/utils/itemUtils.tsx
+++ src/utils/itemUtils.tsx
@@ -1,13 +1,4 @@
 import { Item, ItemAction, ItemType } from "@/types";
-// import { isDatetimeMoreThanOneYearOld } from "./misc";
-// import { DateTime } from "luxon";
-
-// export const calculatePriText = (item: Item): string => {
-//     const isTodoItem = item.type === ItemType.TODO;
-//     return isTodoItem
-//         ? item.content
-//         : item.title;
-// };
 
 export const isValidHexColourCode = (str: string): boolean => {
     const hexColourCodeRegEx = /(^#[A-Fa-f0-9]{6}$)/;
@@ -48,38 +39,3 @@ export const computeItemActionString = (
         return "edit";
     }
 }
-
-
-
-// export const calculateDatetimeStr = (item: Item): string => {
-//     const datetimeDataToUse = item.updated || item.created;
-//     const dateTimeInstance = new Date(datetimeDataToUse);
-
-//     const isMoreThanAYear = isDatetimeMoreThanOneYearOld(dateTimeInstance);
-//     const formatOptions: Intl.DateTimeFormatOptions = {
-//         day: "numeric",
-//         month: "short",
-//         year: isMoreThanAYear
-//             ? "2-digit"
-//             : undefined,
-//     };
-
-//     return Intl.DateTimeFormat("en-US", formatOptions)
-//         .format(dateTimeInstance);
-// };
-
-// type ItemDatetimeProcessOutput = {
-//     relativeDatetime: string | null,
-//     fullDatetime: string,
-// }
-
-// export const processDatetime =(item: Item): ItemDatetimeProcessOutput  => {
-//     const datetimeDataToUse = item.updated || item.created;
-//     // Luxon can't parse this for some reason
-//     const itemDatetime = DateTime.fromJSDate(new Date(datetimeDataToUse));
-    
-//     const relativeDatetime = itemDatetime.toRelative();
-//     const fullDatetime = itemDatetime.toLocaleString(DateTime.DATETIME_FULL);
-
-//     return { relativeDatetime, fullDatetime };
-// }
