b6fcda74f3635ebaea7669497d08826e6ec5a730
Juno Nguyen <juno.ngx@gmail.com>
2024-04-27 20:43:25 +0800

    Disable react-hooks/exhaustive-deps

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -14,6 +14,7 @@ module.exports = {
             'warn',
             { allowConstantExport: true },
         ],
-        "@typescript-eslint/no-unused-vars": "off"
+        "@typescript-eslint/no-unused-vars": "off",
+        "react-hooks/exhaustive-deps": "off",
     },
 }
