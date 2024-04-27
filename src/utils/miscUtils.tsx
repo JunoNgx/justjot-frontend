8b7a52f4173be97cbffbcf480b82ea365299e5a9
Juno Nguyen <juno.ngx@gmail.com>
2024-04-27 19:48:09 +0800

    Remove unused misc utils

--- src/utils/miscUtils.tsx
+++ src/utils/miscUtils.tsx
@@ -1,11 +1,11 @@
-export const isValidUrl = (url: string) => {
-    try { 
-        return Boolean(new URL(url)); 
-    }
-    catch(e){ 
-        return false; 
-    }
-}
+// export const isValidUrl = (url: string) => {
+//     try { 
+//         return Boolean(new URL(url)); 
+//     }
+//     catch(e){ 
+//         return false; 
+//     }
+// }
 
 export const isElementInViewport = (el: HTMLElement): boolean => {
     const rect = el.getBoundingClientRect();
@@ -24,19 +24,19 @@ export const isElementBelowViewport = (el: HTMLBaseElement): boolean => {
     return rect.bottom > (window.innerHeight || document.documentElement.clientHeight);
 }
 
-export const slugify = (str: string) => {
-    return String(str)
-        .normalize('NFKD') // split accented characters into their base characters and diacritical marks
-        .replace(/[\u0300-\u036f]/g, '') // remove all the accents, which happen to be all in the \u03xx UNICODE block.
-        .trim() // trim leading or trailing whitespace
-        .toLowerCase() // convert to lowercase
-        .replace(/\s+/g, '-') // replace spaces with hyphens
-        .replace(/_/g, '') // specifically remove underscore
-        .replace(/[^\w\-]+/g, '') // remove non-alphanumeric characters
-        .replace(/-+/g, '-') // remove consecutive hyphens
-        .replace(/^-+/, '') // trim leading hyphens
-        .replace(/-+$/, ''); // trim trailing hyphens
-}
+// export const slugify = (str: string) => {
+//     return String(str)
+//         .normalize('NFKD') // split accented characters into their base characters and diacritical marks
+//         .replace(/[\u0300-\u036f]/g, '') // remove all the accents, which happen to be all in the \u03xx UNICODE block.
+//         .trim() // trim leading or trailing whitespace
+//         .toLowerCase() // convert to lowercase
+//         .replace(/\s+/g, '-') // replace spaces with hyphens
+//         .replace(/_/g, '') // specifically remove underscore
+//         .replace(/[^\w\-]+/g, '') // remove non-alphanumeric characters
+//         .replace(/-+/g, '-') // remove consecutive hyphens
+//         .replace(/^-+/, '') // trim leading hyphens
+//         .replace(/-+$/, ''); // trim trailing hyphens
+// }
 
 // export const findSelectedIndex = () => {
 //     const itemListWrapper = document.querySelector(`#displayed-list`);
