ec4e8db55f220d7df64130d3bcd2aa125aa78b4c
Juno Nguyen <juno.ngx@gmail.com>
2024-04-27 18:30:08 +0800

    Implement tests for findIndexById

--- src/utils/itemUtils.test.ts
+++ src/utils/itemUtils.test.ts
@@ -1,4 +1,5 @@
-import { isValidHexColourCode } from "./itemUtils";
+import { findIndexById, isValidHexColourCode } from "./itemUtils";
+import * as items from "@/tests/fixtures/items.json";
 
 describe("itemUtils", () => {
     describe("isValidHexColourCode", () => {
@@ -11,5 +12,15 @@ describe("itemUtils", () => {
         test("An invalid hex colour code", () => {
             expect(isValidHexColourCode("#123ABG")).toBe(false);
         })
+    });
+
+    describe("findIndexById", () => {
+        test("Normal condition", () => {
+            expect(findIndexById("475ati3yh4v15qy", items)).toBe(2);
+        });
+
+        test("No item found", () => {
+            expect(findIndexById("zzzzzzz", items)).toBe(-1);
+        });
     });
 });
