f4de7f936130322273cc4b2d8778fc2a35267f37
Juno Nguyen <juno.ngx@gmail.com>
2024-04-27 16:28:41 +0800

    Implement tests for isValidHexColourCode

---
+++ src/utils/itemUtils.test.ts
@@ -0,0 +1,13 @@
+import { isValidHexColourCode } from "./itemUtils";
+
+describe("isValidHexColourCode", () => {
+    test("A valid hex colour code", () => {
+        expect(isValidHexColourCode("#123ABC")).toBe(true);
+    })
+    test("A blank string", () => {
+        expect(isValidHexColourCode("")).toBe(false);
+    })
+    test("An invalid hex colour code", () => {
+        expect(isValidHexColourCode("#123ABG")).toBe(false);
+    })
+});
