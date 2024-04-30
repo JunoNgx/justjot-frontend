88b18ca648b889bbaa415083ec73708d6c1439e6
Juno Nguyen <juno.ngx@gmail.com>
2024-04-30 22:41:01 +0800

    Remove slash character from help spotlight action

--- src/components/SpotlightSearch.tsx
+++ src/components/SpotlightSearch.tsx
@@ -42,7 +42,7 @@ export default function SpotlightSearch() {
         actions: [
             {
                 id: "help",
-                label: "Help / User Manual",
+                label: "Help (User Manual)",
                 description: "/help-man",
                 leftSection: <IconHelp {...spotlightIconProps} />,
                 onClick: navigateToHelp,
