155f016cf365844d1c6aa5f5eb9fb22fae62e59a
Juno Nguyen <juno.ngx@gmail.com>
2024-05-01 17:30:52 +0800

    Implement e2e tests for theme mode

---
+++ e2e/themeModes.test.ts
@@ -0,0 +1,76 @@
+import { test, expect } from '@playwright/test';
+
+test.describe("Theme modes", () => {
+    const spotlightTextboxSelector = 'input.mantine-Spotlight-search';
+
+    test.beforeEach(async ({ page }) => {
+        await page.goto('/');
+    });
+
+    test.describe("System mode", () => {
+
+        test.describe("With header button", () => {
+            test.beforeEach(async ({ page }) => {
+                await page.getByLabel('Switch theme mode to follow').click();
+            });
+    
+            test("Light mode", async ({ page }) => {
+                await page.emulateMedia({ colorScheme: "light" });
+                await expect(page.locator("body")).toHaveCSS("background-color", "rgb(255, 255, 255)");
+            });
+    
+            test("Dark mode", async ({ page }) => {
+                await page.emulateMedia({ colorScheme: "dark" });
+                await expect(page.locator("body")).toHaveCSS("background-color", "rgb(0, 0, 0)");
+            });
+        });
+
+        test.describe("With spotlight action", () => {
+
+            test.beforeEach(async ({ page }) => {
+                await page.locator('body').press('Control+k');
+                await page.locator(spotlightTextboxSelector).fill('.theme-system');
+                await page.locator(spotlightTextboxSelector).press('Enter');
+            });
+    
+            test("Light mode", async ({ page }) => {
+                await page.emulateMedia({ colorScheme: "light" });
+                await expect(page.locator("body")).toHaveCSS("background-color", "rgb(255, 255, 255)");
+            });
+    
+            test("Dark mode", async ({ page }) => {
+                await page.emulateMedia({ colorScheme: "dark" });
+                await expect(page.locator("body")).toHaveCSS("background-color", "rgb(0, 0, 0)");
+            });
+        });
+
+    });
+
+    test.describe("Light mode", () => {
+        test("With header button", async ({ page }) => {
+            await page.getByLabel('Switch theme mode to light').click();
+            await expect(page.locator("body")).toHaveCSS("background-color", "rgb(255, 255, 255)");
+        });
+
+        test("With spotlight action", async ({ page }) => {
+            await page.locator('body').press('Control+k');
+            await page.locator(spotlightTextboxSelector).fill('.theme-light');
+            await page.locator(spotlightTextboxSelector).press('Enter');
+            await expect(page.locator("body")).toHaveCSS("background-color", "rgb(255, 255, 255)");
+        });
+    });
+
+    test.describe("Dark mode", () => {
+        test("With header button", async ({ page }) => {
+            await page.getByLabel('Switch theme mode to dark').click();
+            await expect(page.locator("body")).toHaveCSS("background-color", "rgb(0, 0, 0)");
+        });
+
+        test("With spotlight action", async ({ page }) => {
+            await page.locator('body').press('Control+k');
+            await page.locator(spotlightTextboxSelector).fill('.theme-dark');
+            await page.locator(spotlightTextboxSelector).press('Enter');
+            await expect(page.locator("body")).toHaveCSS("background-color", "rgb(0, 0, 0)");
+        });
+    });
+});
