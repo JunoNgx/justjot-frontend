75cb86961c2371893bfe17f757c7e0e3d955c916
Juno Nguyen <juno.ngx@gmail.com>
2024-05-01 16:44:42 +0800

    Implement e2e tests for accessing test account

---
+++ e2e/accessTestAccount.spec.ts
@@ -0,0 +1,28 @@
+import { test, expect } from '@playwright/test';
+
+test('Accessing test account', async ({ page }) => {
+    await page.goto('http://localhost:4173/');
+
+    await expect(page.getByText('Just want to take a look?Account-free live demo availableTry now')).toBeVisible();
+    await expect(page.getByRole('link', { name: 'Try now' })).toBeVisible();
+
+    await page.getByRole('link', { name: 'Try now' }).click();
+
+    await expect(page.getByText('Try using the test account')).toBeVisible();
+    await expect(page.getByPlaceholder('lucatiel@mirrah.com')).toHaveValue('JayDoeTest');
+    await expect(page.getByPlaceholder('BearSeekSeekLest')).toHaveValue('password123');
+
+    await page.getByRole('button', { name: 'Login' }).click();
+
+    // Test account default data
+    await expect(page.getByText('You are using the test')).toBeVisible();
+    await expect(page.getByRole('button', { name: 'Buy bacon 16:' })).toBeVisible();
+    await expect(page.getByLabel('Link to this item content')).toBeVisible();
+
+    // Switch collection
+    await page.getByRole('button', { name: 'Personal' }).click();
+    await expect(page.getByRole('menuitem', { name: 'Work' })).toBeVisible();
+
+    await page.getByRole('menuitem', { name: 'Work' }).click();
+    await expect(page.getByRole('button', { name: 'Hexcode validator func export' })).toBeVisible();
+});
