0d38bc43d3c0ebc955dac68812d5f47cc6f6050c
Juno Nguyen <juno.ngx@gmail.com>
2024-05-01 16:23:53 +0800

    Add e2e test for public routes using Spotlight

---
+++ e2e/publicRoutesSpotlight.spec.ts
@@ -0,0 +1,36 @@
+import { test, expect } from '@playwright/test';
+
+test('Navigating public routes, using Spotlight', async ({ page }) => {
+    const spotlightTextboxSelector = 'input.mantine-Spotlight-search';
+
+    await page.goto('http://localhost:4173/');
+
+    await page.locator('body').press('Control+k');
+    await expect(page.locator(spotlightTextboxSelector)).toBeVisible();
+
+    await page.locator(spotlightTextboxSelector).fill('login');
+    await page.locator(spotlightTextboxSelector).press('Enter');
+    await expect(page.getByRole('heading', { name: 'Login' })).toBeVisible();
+
+    await page.locator('body').press('Control+k');
+    await page.locator(spotlightTextboxSelector).fill('regist');
+    await page.locator(spotlightTextboxSelector).press('Enter');
+    await expect(page.getByRole('heading', { name: 'Register' })).toBeVisible();
+
+    await page.locator('body').press('Control+k');
+    await page.locator(spotlightTextboxSelector).fill('reset');
+    await page.locator(spotlightTextboxSelector).press('Enter');
+    await expect(page.getByRole('heading', { name: 'Request password change' })).toBeVisible();
+
+    await page.locator('body').press('Control+k');
+    await page.locator(spotlightTextboxSelector).fill('verify');
+    await page.locator(spotlightTextboxSelector).press('Enter');
+    await expect(page.getByRole('heading', { name: 'Request email verification' })).toBeVisible();
+
+    await page.locator('body').press('Control+k');
+    await page.locator(spotlightTextboxSelector).fill('demo');
+    await page.locator(spotlightTextboxSelector).press('Enter');
+    await expect(page.getByRole('heading', { name: 'Login' })).toBeVisible();
+    await expect(page.getByPlaceholder('lucatiel@mirrah.com')).toHaveValue('JayDoeTest');
+    await expect(page.getByPlaceholder('BearSeekSeekLest')).toHaveValue('password123');
+});
