8e93710f707e38f9035e70dd628292da9498f96d
Juno Nguyen <juno.ngx@gmail.com>
2024-05-03 23:36:36 +0800

    Implement tests for collection menu options visibility

--- playwright-tests/collectionMenu.spec.ts
+++ playwright-tests/collectionMenu.spec.ts
@@ -1,18 +1,33 @@
 import { test, expect } from '@playwright/test';
+import fetchItemsEmpty from "./mocks/fetchItemsEmpty.json" assert { type: "json" };
+import { loginWithMocks } from './_common';
 
 test.describe("Collection Menu", () => {
 
-    test.beforeEach(async ({ page }) => {
-
-    });
-
     test.describe("Options visibility", () => {
-        test.fixme("Normal collection", async ({ page }) => {
+        test.beforeEach(loginWithMocks);
 
+        test("Normal collection", async ({ page }) => {
+            await page.route("*/**/api/collections/items/records?page=1&perPage=500&skipTotal=1&filter=collection%3D%226qt1usrvke0tuac%22%20%26%26%20isTrashed%3Dfalse&sort=-created", async route => {
+                await route.fulfill({ json: fetchItemsEmpty });
+            });
+
+            await page.locator("header .collection-menu-btn").click();
+
+            await expect(page.getByRole('menuitem', { name: 'Create collection' })).toBeVisible();
+            await expect(page.getByRole('menuitem', { name: 'Edit collection' })).toBeVisible();
+            await expect(page.getByRole('menuitem', { name: 'Sort collections' })).toBeVisible();
+            await expect(page.getByRole('menuitem', { name: 'Delete collection' })).toBeVisible();
         });
 
-        test.fixme("Trash bin", async ({ page }) => {
+        test("Trash bin", async ({ page }) => {
+            await page.locator("header .collection-menu-btn").click();
+            await page.getByRole('menuitem', { name: 'Trash bin' }).click();
 
+            await expect(page.getByRole('menuitem', { name: 'Create collection' })).toBeVisible();
+            await expect(page.getByRole('menuitem', { name: 'Edit collection' })).toBeVisible();
+            await expect(page.getByRole('menuitem', { name: 'Sort collections' })).toBeVisible();
+            await expect(page.getByRole('menuitem', { name: 'Delete collection' })).not.toBeVisible();
         });
     });
 
