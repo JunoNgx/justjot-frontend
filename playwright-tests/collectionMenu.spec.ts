df4be8adde741a47f4d7489d4f5c5fba932f25d7
Juno Nguyen <juno.ngx@gmail.com>
2024-05-03 22:22:29 +0800

    Draft test list for collection menu

---
+++ playwright-tests/collectionMenu.spec.ts
@@ -0,0 +1,40 @@
+import { test, expect } from '@playwright/test';
+
+test.describe("Collection Menu", () => {
+
+    test.beforeEach(async ({ page }) => {
+
+    });
+
+    test.describe("Options visibility", () => {
+        test.fixme("Normal collection", async ({ page }) => {
+
+        });
+
+        test.fixme("Trash bin", async ({ page }) => {
+
+        });
+    });
+
+    test.describe("Functionalities", () => {
+        test.fixme("Switch collection", async ({ page }) => {
+
+        });
+
+        test.fixme("Create collection", async ({ page }) => {
+
+        });
+
+        test.fixme("Edit collection", async ({ page }) => {
+
+        });
+
+        test.fixme("Sort collection", async ({ page }) => {
+
+        });
+
+        test.fixme("Delete collection", async ({ page }) => {
+
+        });
+    });
+});
