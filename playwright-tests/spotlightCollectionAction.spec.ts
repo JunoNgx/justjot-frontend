0e73a7e49b28093b63db8dd8fdf2cc8488771e74
Juno Nguyen <juno.ngx@gmail.com>
2024-05-03 22:27:43 +0800

    Add todo list for Spotlight collection action test suite

---
+++ playwright-tests/spotlightCollectionAction.spec.ts
@@ -0,0 +1,40 @@
+import { test, expect } from '@playwright/test';
+
+test.describe("Spotlight collection action", () => {
+
+    test.beforeEach(async ({ page }) => {
+
+    });
+
+     test.describe("Options visibility", () => {
+        test.fixme("Note without title", async ({ page }) => {
+
+        });
+
+        test.fixme("Note with title", async ({ page }) => {
+
+        });
+    });
+
+    test.describe("Test keyboard shortcuts", () => {
+        test.fixme("Switch collection", async ({ page }) => {
+
+        });
+
+        test.fixme("Create collection", async ({ page }) => {
+
+        });
+
+        test.fixme("Edit collection", async ({ page }) => {
+
+        });
+
+        test.fixme("Sort collection", async ({ page }) => {
+
+        });
+
+        test.fixme("Delete collection", async ({ page }) => {
+
+        });
+    });
+});
