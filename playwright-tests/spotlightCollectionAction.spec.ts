9af421f83769d7478e05e722c7caa385d17719c5
Juno Nguyen <juno.ngx@gmail.com>
2024-05-04 00:23:42 +0800

    Implement spotlight trash bin test

--- playwright-tests/spotlightCollectionAction.spec.ts
+++ playwright-tests/spotlightCollectionAction.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 import fetchItemsEmpty from "./mocks/fetchItemsEmpty.json" assert { type: "json" };
-import { loginWithMocks } from './_common';
+import { loginWithMocks, spotlightTextboxSelector } from './_common';
 import collectionNew from "./mocks/collectionNew.json" assert { type: "json" };
 import collectionEdit from "./mocks/collectionEdit.json" assert { type: "json" };
 
@@ -21,8 +21,20 @@ test.describe("Spotlight collection action", () => {
             await expect(page.getByRole('button', { name: 'Delete current collection .' })).toBeVisible();
         });
 
-        test.fixme("Note with title", async ({ page }) => {
+        test("Trash bin", async ({ page }) => {
+            await page.locator('body').press('Control+k');
+            await page.locator(spotlightTextboxSelector).fill('/trash-bin');
+            await page.locator(spotlightTextboxSelector).press('Enter');
 
+            await page.locator('body').press('Control+k');
+
+            await expect(page.getByRole('button', { name: 'Logbook /logbook' })).toBeVisible();
+            await expect(page.getByRole('button', { name: 'Coll2 /coll-' })).toBeVisible();
+            await expect(page.getByRole('button', { name: 'Trash bin /trash-bin' })).toBeVisible();
+            await expect(page.getByRole('button', { name: 'Create new collection .create' })).toBeVisible();
+            await expect(page.getByRole('button', { name: 'Edit current collection .edit' })).toBeVisible();
+            await expect(page.getByRole('button', { name: 'Sort collections .sort-coll' })).toBeVisible();
+            await expect(page.getByRole('button', { name: 'Delete current collection .' })).not.toBeVisible();
         });
     });
 
