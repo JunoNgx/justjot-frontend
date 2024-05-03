83a36f21b45ff350a1864caa72d60d99719a3a7b
Juno Nguyen <juno.ngx@gmail.com>
2024-05-03 21:45:35 +0800

    Draft tests for main input

---
+++ playwright-tests/mainInputActions.specs.ts
@@ -0,0 +1,35 @@
+import { test, expect } from '@playwright/test';
+
+test.describe("Main input", () => {
+
+    test.beforeEach(async ({ page }) => {
+
+    });
+
+    test.describe("General functions", () => {
+        test.fixme("Typing changes the main input value", async ({ page }) => {
+
+        });
+
+        test.fixme("Extended menu: with title", async ({ page }) => {
+
+        });
+
+        test.fixme("Extended menu: as todo", async ({ page }) => {
+
+        });
+
+        test.fixme("Extended menu: from clipboard", async ({ page }) => {
+
+        });
+
+        test.fixme("Extended menu: clear input", async ({ page }) => {
+
+        });
+
+        test.fixme("Extended menu: open spotlight", async ({ page }) => {
+
+        });
+    });
+
+});
