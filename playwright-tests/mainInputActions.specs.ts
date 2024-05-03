57c2f27de774e2f07d9e3328ae6b0becc9ed0660
Juno Nguyen <juno.ngx@gmail.com>
2024-05-03 21:59:06 +0800

    Draft test list for keyboard item actions

--- playwright-tests/mainInputActions.specs.ts
+++ playwright-tests/mainInputActions.specs.ts
@@ -32,4 +32,61 @@ test.describe("Main input", () => {
         });
     });
 
+    test.describe("Test keyboard shortcuts", () => {
+        test.fixme("Focus on main input", async ({ page }) => {
+
+        });
+
+        test.fixme("Select items with arrow keys", async ({ page }) => {
+
+        });
+
+        test.fixme("Primary action: edit a note", async ({ page }) => {
+
+        });
+
+        test.fixme("Primary action: open a link", async ({ page }) => {
+
+        });
+
+        test.fixme("Primary action: copy a note content that was marked to copy", async ({ page }) => {
+
+        });
+
+        test.fixme("Primary action: copy a link content that was marked to copy", async ({ page }) => {
+
+        });
+
+        test.fixme("Primary action: toggling a todo isDone status", async ({ page }) => {
+
+        });
+
+        test.fixme("Edit item", async ({ page }) => {
+
+        });
+
+        test.fixme("Move item", async ({ page }) => {
+
+        });
+
+        test.fixme("Trash item", async ({ page }) => {
+
+        });
+
+        test.fixme("Restore trashed item", async ({ page }) => {
+
+        });
+
+        test.fixme("Toggle shouldCopyOnClick", async ({ page }) => {
+
+        });
+
+        test.fixme("Refetch link metadata", async ({ page }) => {
+
+        });
+
+        test.fixme("Convert title-less note to todo", async ({ page }) => {
+
+        });
+    });
 });
