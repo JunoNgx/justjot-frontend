6dea1d593da3b98d1e0c4e396229e35d9c7a1949
Juno Nguyen <juno.ngx@gmail.com>
2024-05-03 22:12:52 +0800

    Draft test suite for item context menu

---
+++ playwright-tests/itemContextMenu.spec.ts
@@ -0,0 +1,66 @@
+import { test, expect } from '@playwright/test';
+
+test.describe("Item context menu", () => {
+
+    test.beforeEach(async ({ page }) => {
+
+    });
+
+    test.describe("Options visibility", () => {
+        test.fixme("Note without title", async ({ page }) => {
+
+        });
+
+        test.fixme("Note with title", async ({ page }) => {
+
+        });
+
+        test.fixme("Link", async ({ page }) => {
+
+        });
+
+        test.fixme("Trashed note", async ({ page }) => {
+
+        });
+    });
+
+    test.describe("Functionalities", () => {
+        test.fixme("Copy", async ({ page }) => {
+
+        });
+
+        test.fixme("Edit", async ({ page }) => {
+
+        });
+
+        test.fixme("Move", async ({ page }) => {
+
+        });
+
+        test.fixme("Trash", async ({ page }) => {
+
+        });
+
+        test.fixme("Delete", async ({ page }) => {
+
+        });
+
+        test.fixme("Restore", async ({ page }) => {
+
+        });
+
+        test.fixme("Refetch", async ({ page }) => {
+
+        });
+
+        test.fixme("Convert to Todo", async ({ page }) => {
+
+        });
+
+        test.fixme("Toggle should copy on click", async ({ page }) => {
+
+        });
+
+    });
+
+});
