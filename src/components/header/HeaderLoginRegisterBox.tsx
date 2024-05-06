aa31c8fb6f594b2cd24b0d8154062ac22a3e6f79
Juno Nguyen <juno.ngx@gmail.com>
2024-05-06 22:53:57 +0800

    Purge mantine from login register box

--- src/components/header/HeaderLoginRegisterBox.tsx
+++ src/components/header/HeaderLoginRegisterBox.tsx
@@ -1,10 +1,9 @@
-import { Anchor, Group, Text } from "@mantine/core";
 import { NavLink } from "react-router-dom";
 
 export default function HeaderLoginRegisterBox() {
-    return <Group>
-        <Anchor component={NavLink} to="login">Login</Anchor>
-        <Text>/</Text>
-        <Anchor component={NavLink} to="register">Register</Anchor>
-    </Group>
+    return <div className="header__LoginRegisterBox">
+        <NavLink to="login">Login</NavLink>
+        <span>/</span>
+        <NavLink to="register">Register</NavLink>
+    </div>
 }
