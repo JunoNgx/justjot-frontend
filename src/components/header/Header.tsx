7357f6ca856a0399f3bd61339a0fd1a8f29e66cf
Juno Nguyen <juno.ngx@gmail.com>
2024-05-06 20:28:50 +0800

    Move header styles to separated css file

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,6 +1,7 @@
 import { Container, Group } from "@mantine/core";
 import HeaderLeftSide from "./HeaderLeftSide";
 import HeaderRightSide from "./HeaderRightSide";
+import "./header.scss";
 
 function Header() {
     return <Container className="header" pl={0} pr={5}>
