263230c0b7c0d4a49cde6f4628527a2cf6c8764f
Juno Nguyen <juno.ngx@gmail.com>
2024-05-10 18:46:35 +0800

    Fix classnames in Landing Page

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -5,6 +5,8 @@ import { Button, Center, Group, Paper, Text, Title } from "@mantine/core";
 import { JustJotIcon } from "@/components/misc/JustJotIcon";
 import { APP_NAME } from "@/utils/constants";
 
+import "./LandingPage.scss";
+
 export default function LandingPage() {
     const { pbClient, isLoggedIn } = useContext(BackendClientContext);
 
@@ -22,8 +24,8 @@ export default function LandingPage() {
 
     const navigate = useNavigate();
 
-    return <div className="landing-page" mb="xl">
-        <section className="landing-page__section">
+    return <div className="LandingPage">
+        <section className="LandingPage__Section">
             <Group gap="xl">
                 <JustJotIcon size={72} stroke={"0.1"}/>
                 <div>
@@ -35,15 +37,13 @@ export default function LandingPage() {
             </Group>
         </section>
 
-        <section className="landing-page__section">
+        <section className="LandingPage__Section">
             <h1>JustJot</h1>
             <Text>A minimalist keyboard-first note-taking Progressive Web App, tailored for fast operations.</Text>
         </section>
 
-        <section className="landing-page__section">
-            <Paper className="landing-page__demo-box"
-                withBorder p="md"
-            >
+        <section className="LandingPage__Section">
+            <Paper className="LandingPage__DemoBox">
                 <Text ta="center">Just want to take a look?</Text>
                 <Text ta="center">Account-free live demo available</Text>
                 <Center mt="md">
@@ -54,14 +54,14 @@ export default function LandingPage() {
             </Paper>
         </section>
 
-        <section className="landing-page__section">
+        <section className="LandingPage__Section">
             <h2>Keyboard-first</h2>
             <Text>JustJot prioritises keyboard interactions; most actions can be performed solely on a keyboard. While fully operable with pointer-based devices (mouse or touchscreen), users are recommended to spend time on learning to perform operations the optimal way.</Text>
 
             <Text mt="md">Due to the unconventional approach, consulting <NavLink to="/help">the manual</NavLink> is highly recommended for new users.</Text>
         </section>
 
-        <section className="landing-page__section">
+        <section className="LandingPage__Section">
             <h2>Your omnipotent notebook</h2>
             <Text>Outside of handling plain-text notes, this app is also capable of:</Text>
             <ul>
@@ -71,19 +71,19 @@ export default function LandingPage() {
             </ul>
         </section>
         
-        <section className="landing-page__section">
+        <section className="LandingPage__Section">
             <h2>Average FOSS software</h2>
             <Text>JustJot was made with personal use and personal preferences first and foremost. It is as barebone and small as it can and should be. It has minimal development roadmap, no monetisation, no advertisement, no tracking, and is fully open-source. Technical support is limited and not guaranteed (I'll try as much as I can).</Text>
         </section>
 
-        <section className="landing-page__section">
+        <section className="LandingPage__Section">
             <h2>Get started</h2>
             <Text><NavLink to="/register">Register</NavLink> a new account or <NavLink to="/login">login</NavLink> to your existing one.</Text>
             <Text mt="md">Also, you don't need to sign up just to see what this is all about. There is a <NavLink to="/demo-login">live demo</NavLink> using a test account, obligation-free.</Text>
             <Text mt="md">While we are at that, drop by to see <NavLink to="/terms">things you should know about</NavLink> and <NavLink to="/help">the manual</NavLink> before using JustJot.</Text>
         </section>
 
-        <section className="landing-page__section">
+        <section className="LandingPage__Section">
             <h2>Authors</h2>
             <Text>JustJot is developed by <a href="https://www.junongx.com/" target='_blank' rel='noopener noreferrer'>Juno Nguyen</a> with curiosity, compulsion, and tons of design input from <a href="https://caseykwokdinata.webflow.io/" target='_blank' rel='noopener noreferrer'>Casey Kwokdinata</a>.</Text>
         </section>
