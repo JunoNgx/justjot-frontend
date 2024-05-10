ed3c2839be5bb39118ac70244e53518ae271ec30
Juno Nguyen <juno.ngx@gmail.com>
2024-05-10 19:20:15 +0800

    Fix classes in Landing Pages further

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -29,41 +29,45 @@ export default function LandingPage() {
             <Group gap="xl">
                 <JustJotIcon size={72} stroke={"0.1"}/>
                 <div>
-                    <h2>jot</h2>
-                    <Text fs="italic">/dʒɒt/</Text>
-                    <Text>(verb)</Text>
-                    <Text>write (something) quickly</Text>
+                    <h2 className="LandingPage__DefText">jot</h2>
+                    <p className="LandingPage__DefText LandingPage__DefText--IsItalic">/dʒɒt/</p>
+                    <p className="LandingPage__DefText">(verb)</p>
+                    <p className="LandingPage__DefText">write (something) quickly</p>
                 </div>
             </Group>
         </section>
 
         <section className="LandingPage__Section">
             <h1>JustJot</h1>
-            <Text>A minimalist keyboard-first note-taking Progressive Web App, tailored for fast operations.</Text>
+            <p>A minimalist keyboard-first note-taking Progressive Web App, tailored for fast operations.</p>
         </section>
 
         <section className="LandingPage__Section">
-            <Paper className="LandingPage__DemoBox">
-                <Text ta="center">Just want to take a look?</Text>
-                <Text ta="center">Account-free live demo available</Text>
+            <div className="LandingPage__DemoBox">
+                <p className="LandingPage__DemoText">Just want to take a look?</p>
+                <p  className="LandingPage__DemoText">Account-free live demo available</p>
                 <Center mt="md">
-                    <Button component={NavLink} to="/demo-login">
+                    <NavLink className="LandingPage__DemoCtaBtn"
+                        to="/demo-login"
+                    >
                         Try now
-                    </Button>
+                    </NavLink>
                 </Center>
-            </Paper>
+            </div>
         </section>
 
         <section className="LandingPage__Section">
             <h2>Keyboard-first</h2>
-            <Text>JustJot prioritises keyboard interactions; most actions can be performed solely on a keyboard. While fully operable with pointer-based devices (mouse or touchscreen), users are recommended to spend time on learning to perform operations the optimal way.</Text>
+            <p>JustJot prioritises keyboard interactions; most actions can be performed solely on a keyboard. While fully operable with pointer-based devices (mouse or touchscreen), users are recommended to spend time on learning to perform operations the optimal way.</p>
 
-            <Text mt="md">Due to the unconventional approach, consulting <NavLink to="/help">the manual</NavLink> is highly recommended for new users.</Text>
+            <p>Due to the unconventional approach, consulting <NavLink to="/help">the manual</NavLink> is highly recommended for new users.</p>
         </section>
 
         <section className="LandingPage__Section">
             <h2>Your omnipotent notebook</h2>
-            <Text>Outside of handling plain-text notes, this app is also capable of:</Text>
+            <p className="LandingPage__UlPredecessor">
+                Outside of handling plain-text notes, this app is also capable of:
+            </p>
             <ul>
                 <li>Storing bookmarks</li>
                 <li>Managing todo items</li>
@@ -73,19 +77,19 @@ export default function LandingPage() {
         
         <section className="LandingPage__Section">
             <h2>Average FOSS software</h2>
-            <Text>JustJot was made with personal use and personal preferences first and foremost. It is as barebone and small as it can and should be. It has minimal development roadmap, no monetisation, no advertisement, no tracking, and is fully open-source. Technical support is limited and not guaranteed (I'll try as much as I can).</Text>
+            <p>JustJot was made with personal use and personal preferences first and foremost. It is as barebone and small as it can and should be. It has minimal development roadmap, no monetisation, no advertisement, no tracking, and is fully open-source. Technical support is limited and not guaranteed (I'll try as much as I can).</p>
         </section>
 
         <section className="LandingPage__Section">
             <h2>Get started</h2>
-            <Text><NavLink to="/register">Register</NavLink> a new account or <NavLink to="/login">login</NavLink> to your existing one.</Text>
-            <Text mt="md">Also, you don't need to sign up just to see what this is all about. There is a <NavLink to="/demo-login">live demo</NavLink> using a test account, obligation-free.</Text>
-            <Text mt="md">While we are at that, drop by to see <NavLink to="/terms">things you should know about</NavLink> and <NavLink to="/help">the manual</NavLink> before using JustJot.</Text>
+            <p><NavLink to="/register">Register</NavLink> a new account or <NavLink to="/login">login</NavLink> to your existing one.</p>
+            <p>Also, you don't need to sign up just to see what this is all about. There is a <NavLink to="/demo-login">live demo</NavLink> using a test account, obligation-free.</p>
+            <p>While we are at that, drop by to see <NavLink to="/terms">things you should know about</NavLink> and <NavLink to="/help">the manual</NavLink> before using JustJot.</p>
         </section>
 
         <section className="LandingPage__Section">
             <h2>Authors</h2>
-            <Text>JustJot is developed by <a href="https://www.junongx.com/" target='_blank' rel='noopener noreferrer'>Juno Nguyen</a> with curiosity, compulsion, and tons of design input from <a href="https://caseykwokdinata.webflow.io/" target='_blank' rel='noopener noreferrer'>Casey Kwokdinata</a>.</Text>
+            <p>JustJot is developed by <a href="https://www.junongx.com/" target='_blank' rel='noopener noreferrer'>Juno Nguyen</a> with curiosity, compulsion, and tons of design input from <a href="https://caseykwokdinata.webflow.io/" target='_blank' rel='noopener noreferrer'>Casey Kwokdinata</a>.</p>
         </section>
     </div>
 }
