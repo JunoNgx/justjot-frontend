5691c51a9dd2afa9ed28735f893d7ca088430286
Juno Nguyen <juno.ngx@gmail.com>
2024-05-11 16:01:39 +0800

    Update classnames for change password

--- src/components/profile/ProfileChangePassword.tsx
+++ src/components/profile/ProfileChangePassword.tsx
@@ -1,17 +1,11 @@
-import { Anchor, Paper, Text, Title } from "@mantine/core";
 import { Link } from "react-router-dom";
 
 export default function ProfileChangePassword() {
-    return <Paper className="cardlike"
-        withBorder
-        p="md"
-    >
-        <Title className="cardlike__title"
-            order={2}
-        >
+    return <div className="Profile">
+        <h3 className="Profile__Title">
             Change password
-        </Title>
+        </h3>
         
-        <Text>For password changes, please use the <Anchor component={Link} to="/reset">password reset form</Anchor>.</Text>
-    </Paper>
+        <p>For password changes, please use the <Link to="/reset">password reset form</Link>.</p>
+    </div>
 }
