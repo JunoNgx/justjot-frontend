04b6d6a39ed46f350bea86bbf5b55d557d82b1ba
Juno Nguyen <juno.ngx@gmail.com>
2024-05-11 21:07:39 +0800

    Rename attribute to lower case

--- src/libs/components/Loader.tsx
+++ src/libs/components/Loader.tsx
@@ -4,6 +4,6 @@ export default function Loader(
     { shouldUsePriCol = true }: { shouldUsePriCol?: boolean }
 ) {
     return <div className="Loader"
-        data-usePriCol={shouldUsePriCol}
+        data-use-primary-color={shouldUsePriCol}
     />
 }
