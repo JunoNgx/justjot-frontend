b9a5f9f77ea976000ed46a56e6ce439524c39b68
Juno Nguyen <juno.ngx@gmail.com>
2024-05-11 22:59:38 +0800

    Remove unused import

--- src/components/header/HeaderUser.tsx
+++ src/components/header/HeaderUser.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect } from "react";
+import { useContext } from "react";
 import { BackendClientContext } from "@/contexts/BackendClientContext";
 import { IconChevronDown, IconHelp, IconLogout, IconUserCog } from "@tabler/icons-react";
 import useIconProps from "@/hooks/useIconProps";
