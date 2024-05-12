b84b972bdac1c71468438e82f0eafb9ca7b2bc0f
Juno Nguyen <juno.ngx@gmail.com>
2024-05-12 11:49:14 +0800

    Add incomplete todos syntax

--- src/utils/constants.tsx
+++ src/utils/constants.tsx
@@ -6,9 +6,11 @@ export const AUTO_CLOSE_DEFAULT = 3000;
 export const COLLECTION_SORT_ORDER_MAG = 1000;
 export const MAX_TITLE_LENGTH = 200;
 export const MAX_CONTENT_LENGTH = 10000;
+
 export const CREATE_TEXT_WITH_TITLE_PREFIX = ":t:";
 export const CREATE_TEXT_WITH_TITLE_PREFIX_ALT = ":l:";
 export const CREATE_TODO_PREFIX = ":td:";
+export const INCOMPLETE_TODOS_SYNTAX = "::itd::";
 
 export const TEST_ACC_USERNAME = "JayDoeTest";
 export const TEST_ACC_PASSWORD = "password123";
