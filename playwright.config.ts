c43cd027d6d29860f62ef9b4c6560b24ecb5ffb6
Juno Nguyen <juno.ngx@gmail.com>
2024-05-20 20:45:29 +0800

    Reformat config for playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,72 +10,73 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: './playwright-tests',
-  /* Run tests in files in parallel */
-  fullyParallel: true,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly: !!process.env.CI,
-  retries: 3,
-  // Intentionally opt out of parallel tests to avoid issues caused by endpoint interception
-  workers: 1,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'list',
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+    testDir: './playwright-tests',
+    /* Run tests in files in parallel */
+    fullyParallel: true,
+    /* Fail the build on CI if you accidentally left test.only in the source code. */
+    forbidOnly: !!process.env.CI,
+    retries: 3,
+    // Intentionally opt out of parallel tests to avoid issues caused by endpoint interception
+    workers: 1,
+    /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+    reporter: 'list',
+    /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
 
-  /* Configure projects for major browsers */
-  projects: [
-    {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+    /* Configure projects for major browsers */
+    projects: [
+        {
+            name: 'chromium',
+            use: { ...devices['Desktop Chrome'] },
+        },
+
+        {
+            name: 'firefox',
+            use: { ...devices['Desktop Firefox'] },
+        },
+
+        // {
+        //     name: 'webkit',
+        //     use: { ...devices['Desktop Safari'] },
+        // },
+
+        // TODO
+        /* Test against mobile viewports. */
+        // {
+        //     name: 'Mobile Chrome',
+        //     use: { ...devices['Pixel 5'] },
+        // },
+        // {
+        //     name: 'Mobile Safari',
+        //     use: { ...devices['iPhone 12'] },
+        // },
+
+        /* Test against branded browsers. */
+        // {
+        //     name: 'Microsoft Edge',
+        //     use: { ...devices['Desktop Edge'], channel: 'msedge' },
+        // },
+        // {
+        //     name: 'Google Chrome',
+        //     use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+        // },
+    ],
+
+    /* Run your local dev server before starting the tests */
+    webServer: {
+        command: 'yarn dev',
+        // url: 'http://127.0.0.1:3000',
+        // url: 'http://localhost:4173/',
+        url: 'http://localhost:5173/',
+        reuseExistingServer: !process.env.CI,
+        timeout: 5 * 1000,
     },
+    use: {
+        ignoreHTTPSErrors: true,
+        /* Base URL to use in actions like `await page.goto('/')`. */
+        // baseURL: 'http://localhost:4173/',
+        baseURL: 'http://localhost:5173/',
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
+        /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+        trace: 'on-first-retry',
     },
-
-    // {
-    //   name: 'webkit',
-    //     use: { ...devices['Desktop Safari'] },
-    // },
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
-  ],
-
-  /* Run your local dev server before starting the tests */
-  webServer: {
-    command: 'yarn dev',
-    // url: 'http://127.0.0.1:3000',
-    // url: 'http://localhost:4173/',
-    url: 'http://localhost:5173/',
-    reuseExistingServer: !process.env.CI,
-    timeout: 5 * 1000,
-  },
-  use: {
-    ignoreHTTPSErrors: true,
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://localhost:4173/',
-    baseURL: 'http://localhost:5173/',
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
-  },
 });
