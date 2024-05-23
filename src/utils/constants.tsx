1006886317d849e542f0cdd28008414b2919b6a6
Juno Nguyen <juno.ngx@gmail.com>
2024-05-23 23:49:55 +0800

    Add new filter syntax constants

--- src/utils/constants.tsx
+++ src/utils/constants.tsx
@@ -10,6 +10,10 @@ export const MAX_CONTENT_LENGTH = 10000;
 export const CREATE_TEXT_WITH_TITLE_PREFIX = ":t:";
 export const CREATE_TEXT_WITH_TITLE_PREFIX_ALT = ":l:";
 export const CREATE_TODO_PREFIX = ":td:";
+
+export const FILTER_SYNTAX_NOTES = "::nt::";
+export const FILTER_SYNTAX_LINKS = "::link::";
+export const FILTER_SYNTAX_TODOS = "::td::";
 export const FILTER_SYNTAX_INCOMPLETE_TODOS = "::itd::";
 
 export const TEST_ACC_USERNAME = "JayDoeTest";
