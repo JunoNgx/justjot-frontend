e59b353f29ef2ea5b40829d25c8243c157ab389a
Juno Nguyen <juno.ngx@gmail.com>
2025-02-09 04:50:50 +0800

    #12: reformat linter config

--- eslint.config.js
+++ eslint.config.js
@@ -6,9 +6,13 @@ import pluginReact from "eslint-plugin-react";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.ts", "**/*.tsx"]},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
+    {
+        files: ["**/*.ts", "**/*.tsx"]
+    },
+    {
+        languageOptions: { globals: globals.browser }
+    },
+    pluginJs.configs.recommended,
+    ...tseslint.configs.recommended,
+    pluginReact.configs.flat.recommended,
 ];
