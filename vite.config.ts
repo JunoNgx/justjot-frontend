e5a66937c55659ad6ac86a800fc9bfc813284214
Juno Nguyen <juno.ngx@gmail.com>
2025-02-08 23:57:41 +0800

    #12: restore default _mantine css file

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'node:path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
    plugins: [react()],
    css: {
+      // preprocessorOptions: {
+      //    scss: {
+      //       // TODO: create PR to update Mantine doc, Usage with Sass
+      //       additionalData: `@use "./src/styles/_sassMantine" as mantine;`,
+      //    }
+      // }
       preprocessorOptions: {
          scss: {
-            // TODO: create PR to update Mantine doc, Usage with Sass
-            additionalData: `@use "./src/styles/_sassMantine" as mantine;`,
-         }
-      }
+           api: 'modern-compiler',
+           additionalData: `@use "${path.join(process.cwd(), 'src/_mantine').replace(/\\/g, '/')}" as mantine;`,
+         },
+       },   
    },
    resolve: {
       alias: {
