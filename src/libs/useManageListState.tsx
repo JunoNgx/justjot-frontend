fb1b113c171c5dd96896b446274ff40ce16d1fe8
Juno Nguyen <juno.ngx@gmail.com>
2025-08-15 19:28:28 +0800

    #13: reformat lib files

--- src/libs/useManageListState.tsx
+++ src/libs/useManageListState.tsx
@@ -9,48 +9,49 @@ export default function useManageListState<T>(
     setState: React.Dispatch<React.SetStateAction<T[]>>
 ) {
     const prepend = (item: T) => {
-        setState(curr => [item, ...curr]);
+        setState((curr) => [item, ...curr]);
     };
 
     const append = (item: T) => {
-        setState(curr => [...curr, item]);
+        setState((curr) => [...curr, item]);
     };
 
     const pop = () => {
-        setState(curr => {
+        setState((curr) => {
             const tempArr = [...curr];
             return tempArr.splice(0, tempArr.length - 1);
         });
     };
 
     const remove = (...indices: number[]) => {
-        setState(curr => curr.filter((_, index) => !indices.includes(index)));
+        setState((curr) => curr.filter((_, index) => !indices.includes(index)));
     };
 
     const insert = (index: number, ...items: T[]) => {
-        setState(curr => {
-            return [...curr].splice(0, index + 1)
+        setState((curr) => {
+            return [...curr]
+                .splice(0, index + 1)
                 .concat([...items])
                 .concat([...curr].splice(index + 1));
         });
     };
 
     const replace = (index: number, item: T) => {
-        setState(curr => {
+        setState((curr) => {
             const tempArr = [...curr];
             tempArr[index] = item;
             return tempArr;
         });
-    }
+    };
 
     const replaceProps = (index: number, prop: object) => {
-        setState(curr => {
+        setState((curr) => {
             const tempArr = [...curr];
             const item = tempArr[index];
-            tempArr[index] = {...item, ...prop};
+            tempArr[index] = { ...item, ...prop };
             return tempArr;
         });
-    }
+    };
 
     return {
         prepend,
