789a5ebba12f6447cc20e7d62fb2d63a35628721
Juno Nguyen <juno.ngx@gmail.com>
2025-08-15 19:29:35 +0800

    #13: reformat lib components

--- src/libs/components/LabelWithIcon.tsx
+++ src/libs/components/LabelWithIcon.tsx
@@ -2,12 +2,12 @@ import { ReactNode } from "react";
 import "./LabelWithIcon.scss";
 
 type LabelWithIconOptions = {
-    className?: string,
-    children?: ReactNode,
-    onClick?: () => void,
-    leftSection?: ReactNode,
-    rightSection?: ReactNode,
-    gap?: string,
+    className?: string;
+    children?: ReactNode;
+    onClick?: () => void;
+    leftSection?: ReactNode;
+    rightSection?: ReactNode;
+    gap?: string;
 };
 
 export default function LabelWithIcon({
@@ -17,18 +17,20 @@ export default function LabelWithIcon({
     leftSection,
     rightSection,
     gap = "0.5rem",
-}: LabelWithIconOptions
-) {
-    return <span className={`${className} LabelWithIcon`}
-        onClick={onClick}
-        style={{
-            gap,
-        }}
-    >
-        <span className="LabelWithIcon__LeftSide">
-            {leftSection}
-            <span className="LabelWithIcon__Label">{children}</span>
+}: LabelWithIconOptions) {
+    return (
+        <span
+            className={`${className} LabelWithIcon`}
+            onClick={onClick}
+            style={{
+                gap,
+            }}
+        >
+            <span className="LabelWithIcon__LeftSide">
+                {leftSection}
+                <span className="LabelWithIcon__Label">{children}</span>
+            </span>
+            <span className="LabelWithIcon__RightSide">{rightSection}</span>
         </span>
-        <span className="LabelWithIcon__RightSide">{rightSection}</span>
-    </span>
+    );
 }
--- src/libs/components/Loader.tsx
+++ src/libs/components/Loader.tsx
@@ -1,9 +1,9 @@
-import "./Loader.scss"
+import "./Loader.scss";
 
-export default function Loader(
-    { shouldUsePriCol = true }: { shouldUsePriCol?: boolean }
-) {
-    return <div className="Loader"
-        data-use-primary-color={shouldUsePriCol}
-    />
+export default function Loader({
+    shouldUsePriCol = true,
+}: {
+    shouldUsePriCol?: boolean;
+}) {
+    return <div className="Loader" data-use-primary-color={shouldUsePriCol} />;
 }
