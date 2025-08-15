fa22739fa96bcbea6d51afa1e0fb0d0ccace3c66
Juno Nguyen <juno.ngx@gmail.com>
2025-08-15 19:37:55 +0800

    #13: reformat misc components

--- src/components/misc/CollectionHotkey.tsx
+++ src/components/misc/CollectionHotkey.tsx
@@ -1,10 +1,10 @@
-export default function CollectionHotkey({index}: {index: number}) {
-   switch (true) {
-    case index === 9:
-        return <kbd>0</kbd>
-    case index < 9:
-        return <kbd>{index + 1}</kbd>
-    default:
-        return <div className="CollectionNumberPlaceholder"/>
+export default function CollectionHotkey({ index }: { index: number }) {
+    switch (true) {
+        case index === 9:
+            return <kbd>0</kbd>;
+        case index < 9:
+            return <kbd>{index + 1}</kbd>;
+        default:
+            return <div className="CollectionNumberPlaceholder" />;
     }
 }
